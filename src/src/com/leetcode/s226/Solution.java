7a9760f4517cdad77d8487583d574d17627acf66
jakejakeho <mtho@connect.ust.hk>
2023-03-04 23:47:37 +0800

    s226

---
+++ src/src/com/leetcode/s226/Solution.java
@@ -0,0 +1,26 @@
+package src.com.leetcode.s226;
+
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+
+    }
+}

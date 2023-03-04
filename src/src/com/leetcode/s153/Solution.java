c14d1f7327a7e264e4a1bf0ce30c1c870ff1aff1
jakejakeho <mtho@connect.ust.hk>
2023-03-04 23:47:19 +0800

    s153

--- src/src/com/leetcode/s153/Solution.java
+++ src/src/com/leetcode/s153/Solution.java
@@ -25,21 +25,21 @@ public class Solution {
         Solution solution = new Solution();
 
         // test case 1
-        int[] nums = new int[]{ 3, 4, 5, 1, 2 };
+        int[] nums = new int[]{3, 4, 5, 1, 2};
         int result = solution.findMin(nums);
         int expected = 1;
         boolean correct = result == expected;
         System.out.println("Test case 1 nums = " + Arrays.toString(nums) + " result = " + result + " expected = " + expected + " correct = " + correct);
 
         // test case 2
-        nums = new int[]{ 4, 5, 6, 7, 0, 1, 2 };
+        nums = new int[]{4, 5, 6, 7, 0, 1, 2};
         result = solution.findMin(nums);
         expected = 0;
         correct = result == expected;
         System.out.println("Test case 2 nums = " + Arrays.toString(nums) + " result = " + result + " expected = " + expected + " correct = " + correct);
 
         // test case 3
-        nums = new int[]{ 11, 13, 15, 17 };
+        nums = new int[]{11, 13, 15, 17};
         result = solution.findMin(nums);
         expected = 11;
         correct = result == expected;

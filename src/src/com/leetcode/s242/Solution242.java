6ae15f5e2ce69e8a45a10f74613063b942f57f74
jakejakeho <mtho@connect.ust.hk>
2023-03-05 00:38:18 +0800

    S242

--- src/src/com/leetcode/s242/Solution242.java
+++ src/src/com/leetcode/s242/Solution242.java
@@ -2,20 +2,16 @@ package src.com.leetcode.s242;
 
 public class Solution242 {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-
-        int[] occurrenceS = new int[26];
-        int[] occurrenceT = new int[26];
-        for (char c: s.toCharArray()) {
-            occurrenceS[(c - 'a')]++;
+        if (s.length() != t.length()) return false;
+        int[] occurrence = new int[26];
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        for (int i = 0; i < charS.length; i++) {
+            occurrence[(charS[i] - 'a')]++;
+            occurrence[(charT[i] - 'a')]--;
         }
-        for (char c: t.toCharArray()) {
-            occurrenceT[(c - 'a')]++;
-        }
-
-        for (int i = 0; i < occurrenceS.length;i++) {
-            if (occurrenceS[i] != occurrenceT[i])
+        for (int j : occurrence) {
+            if (j != 0)
                 return false;
         }
         return true;

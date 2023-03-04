5ab16ba132de572288fa3cd6a873858e49b62f0a
jakejakeho <mtho@connect.ust.hk>
2023-03-04 23:47:21 +0800

    s33

--- src/src/com/leetcode/s33/Solution.java
+++ src/src/com/leetcode/s33/Solution.java
@@ -20,15 +20,15 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
+            if (nums[left] <= nums[right]) {
+                return left;
+            }
+
             int mid = (left + right) / 2;
-            int prev = (mid - 1 + nums.length) % nums.length;
-            int next = (mid + 1) % nums.length;
-            if (nums[mid] <= nums[prev] && nums[mid] <= nums[next]) {
-                return mid;
-            } else if (nums[mid] <= nums[right]) {
-                right = mid - 1;
-            } else {
+            if (nums[mid] >= nums[left]) {
                 left = mid + 1;
+            } else {
+                right = mid;
             }
         }
         return -1;

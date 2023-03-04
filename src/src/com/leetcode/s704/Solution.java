930d8f8a6af4d17e94e29cd0898c4c743738d9cf
jakejakeho <mtho@connect.ust.hk>
2023-03-04 23:47:15 +0800

    s704

--- src/src/com/leetcode/s704/Solution.java
+++ src/src/com/leetcode/s704/Solution.java
@@ -1,17 +1,17 @@
 package src.com.leetcode.s704;
 public class Solution {
     public int search(int[] nums, int target) {
-        int leftIndex = 0;
-        int rightIndex = nums.length - 1;
-        while (leftIndex <= rightIndex) {
-           int mid = (leftIndex + rightIndex) / 2;
-           if (target == nums[mid]) {
-               return mid;
-           } else if (target > nums[mid]) {
-               leftIndex = mid + 1;
-           } else {
-               rightIndex = mid - 1;
-           }
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
         return -1;
     }

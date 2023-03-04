4898efd6bfeee7c3c49cde2ab1e55ff23cd1dc54
jakejakeho <mtho@connect.ust.hk>
2023-03-04 23:47:20 +0800

    s33

--- src/src/com/leetcode/s33/Solution.java
+++ src/src/com/leetcode/s33/Solution.java
@@ -3,6 +3,7 @@ package src.com.leetcode.s33;
 import java.util.Arrays;
 
 class Solution {
+
     public int search(int[] nums, int target) {
         int minIndex = minIndex(nums);
         int leftSearch = binarySearch(nums, 0, minIndex - 1, target);
@@ -19,10 +20,10 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int midLeft = (mid - 1 + nums.length) % nums.length;
-            int midRight = (mid + 1) % nums.length;
-            if (nums[mid] <= nums[midLeft] && nums[mid] <= nums[midRight]) {
+            int mid = (left + right) / 2;
+            int prev = (mid - 1 + nums.length) % nums.length;
+            int next = (mid + 1) % nums.length;
+            if (nums[mid] <= nums[prev] && nums[mid] <= nums[next]) {
                 return mid;
             } else if (nums[mid] <= nums[right]) {
                 right = mid - 1;
@@ -32,16 +33,16 @@ class Solution {
         }
         return -1;
     }
+
     public int binarySearch(int[] nums, int start, int end, int target) {
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
-            int midNum = nums[mid];
-            if (midNum == target) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
                 return mid;
-            } else if (midNum < target) {
-                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
             } else {
-                end = end - 1;
+                start = mid + 1;
             }
         }
         return -1;
@@ -50,40 +51,52 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+        int[] nums = new int[] {4, 5, 6, 7, 0, 1, 2};
         int target = 0;
         int result = solution.search(nums, target);
         int expected = 4;
-        System.out.println("nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = " + expected);
+        System.out.println(
+            "nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = "
+                + expected);
 
-        nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+        nums = new int[] {4, 5, 6, 7, 0, 1, 2};
         target = 3;
         result = solution.search(nums, target);
         expected = -1;
-        System.out.println("nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = " + expected);
+        System.out.println(
+            "nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = "
+                + expected);
 
-        nums = new int[]{1};
+        nums = new int[] {1};
         target = 0;
         result = solution.search(nums, target);
         expected = -1;
-        System.out.println("nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = " + expected);
+        System.out.println(
+            "nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = "
+                + expected);
 
-        nums = new int[]{1};
+        nums = new int[] {1};
         target = 1;
         result = solution.search(nums, target);
         expected = 0;
-        System.out.println("nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = " + expected);
+        System.out.println(
+            "nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = "
+                + expected);
 
-        nums = new int[]{1, 3};
+        nums = new int[] {1, 3};
         target = 0;
         result = solution.search(nums, target);
         expected = -1;
-        System.out.println("nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = " + expected);
+        System.out.println(
+            "nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = "
+                + expected);
 
-        nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+        nums = new int[] {4, 5, 6, 7, 0, 1, 2};
         target = 0;
         result = solution.search(nums, target);
         expected = 4;
-        System.out.println("nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = " + expected);
+        System.out.println(
+            "nums = " + Arrays.toString(nums) + " target = " + target + " result = " + result + " expected = "
+                + expected);
     }
 }

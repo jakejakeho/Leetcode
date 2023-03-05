ac878934e9cc88c4dfeee7b89b66fd1c98315aba
jakejakeho <mtho@connect.ust.hk>
2023-03-05 14:17:45 +0800

    s238

--- src/src/com/leetcode/s238/Solution238.java
+++ src/src/com/leetcode/s238/Solution238.java
@@ -1,25 +1,21 @@
 package src.com.leetcode.s238;
 
-import java.util.Arrays;
-
 public class Solution238 {
-
     public int[] productExceptSelf(int[] nums) {
-        int[] results = new int[nums.length];
-        Arrays.fill(results, 1);
-
+        int[] result = new int[nums.length];
         int prefix = 1;
         for (int i = 0; i < nums.length; i++) {
-            results[i] = prefix;
-            prefix *= nums[i];
+            int prefixValue = ((i - 1) < 0) ? 1 : nums[i - 1];
+            prefix *= prefixValue;
+            result[i] = prefix;
         }
 
         int postfix = 1;
         for (int i = nums.length - 1; i >= 0; i--) {
-            results[i] *= postfix;
-            postfix *= nums[i];
+            int postfixValue = ((i + 1) >= nums.length) ? 1 : nums[i + 1];
+            postfix *= postfixValue;
+            result[i] *= postfix;
         }
-
-        return results;
+        return result;
     }
 }

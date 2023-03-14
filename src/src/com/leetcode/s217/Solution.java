4e881b8aaac442e6e48b2df5ccf06a0891436e45
jake.ho <jake.ho@okg.com>
2023-03-14 09:11:27 +0800

    s217

--- src/src/com/leetcode/s217/Solution.java
+++ src/src/com/leetcode/s217/Solution.java
@@ -1,9 +1,12 @@
 package src.com.leetcode.s217;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
+
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num: nums) {
@@ -14,5 +17,26 @@ public class Solution {
             }
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums;
+        boolean expected;
+        boolean result;
+        boolean isCorrect;
+        /**
+         * Input: nums = [1,2,3,1]
+         * Output: true
+         */
+        nums = new int[] {1, 2, 3, 1};
+        expected = true;
+        result = solution.containsDuplicate(nums);
+        isCorrect = expected == result;
+
+        System.out.println("Input: nums = " + Arrays.toString(nums));
+        System.out.println("Output: " + result + " expected = " + expected + " correct = " + isCorrect);
+
     }
 }

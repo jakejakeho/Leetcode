08a14bbb2e6af2166ec1cadebd4e95df6b61eb0d
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:40:45 +0800

    easy

--- src/src/com/leetcode/s128/Solution.java
+++ src/src/com/leetcode/s128/Solution.java
@@ -1,32 +1,50 @@
 package src.com.leetcode.s128;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class Solution {
     public int longestConsecutive(int[] nums) {
-        int longest = 0;
+        int result = 0;
         HashMap<Integer, Boolean> hashMap = new HashMap<>();
-        for (int num : nums) {
-            hashMap.put(num, true);
+        for (int i = 0; i < nums.length; i++) {
+            hashMap.put(nums[i], false);
         }
 
-        for (int num: nums) {
-            int count = 1;
-            int left = num - 1;
-            while(hashMap.getOrDefault(left, false)) {
-                hashMap.put(left, false);
-                count++;
-                left--;
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            if (hashMap.getOrDefault(start, false)) continue;
+            while (!hashMap.getOrDefault(start, false) && hashMap.containsKey(start)) {
+                start--;
             }
-            int right = num + 1;
-            while(hashMap.getOrDefault(right, false)) {
-                hashMap.put(right, false);
-                count++;
-                right++;
+            int smallest = start + 1;
+            int loop = smallest;
+            int length = 0;
+            while (hashMap.containsKey(loop)) {
+                hashMap.put(loop, true);
+                loop++;
+                length++;
             }
-            longest = Math.max(longest, count);
+            result = Math.max(length, result);
         }
-        return longest;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String data = "";
+        try {
+            File myObj = new File("C:\\Users\\Jake\\IdeaProjects\\Leetcode\\src\\src\\com\\leetcode\\s128\\testcase.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                data = myReader.nextLine();
+            }
+            myReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        int[] nums = Arrays.stream(data.split(" ")).mapToInt(Integer::parseInt).toArray();
+        Solution solution = new Solution();
+        System.out.println("Output: " + solution.longestConsecutive(nums));
     }
 }
--- src/src/com/leetcode/s36/Solution.java
+++ src/src/com/leetcode/s36/Solution.java
@@ -1,52 +1,65 @@
 package src.com.leetcode.s36;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            boolean[] existsMap = new boolean[10];
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == '.') continue;
-                int value = board[i][j] - '0';
-                if (existsMap[value]) {
+        for (int i = 0; i < 9; i++) {
+            boolean[] exists = new boolean[9];
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')
+                    continue;
+                if (exists[board[i][j] - '1'])
                     return false;
-                } else {
-                    existsMap[value] = true;
-                }
+                exists[board[i][j] - '1'] = true;
             }
         }
 
-        for (int i = 0; i < board.length; i++) {
-            boolean[] existsMap = new boolean[10];
-            for (int j = 0; j < board.length; j++) {
-                if (board[j][i] == '.') continue;
-                int value = board[j][i] - '0';
-                if (existsMap[value]) {
+        for (int i = 0; i < 9; i++) {
+            boolean[] exists = new boolean[9];
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] == '.')
+                    continue;
+                if (exists[board[j][i] - '1'])
                     return false;
-                } else {
-                    existsMap[value] = true;
-                }
+                exists[board[j][i] - '1'] = true;
             }
         }
 
-        for (int i = 0; i < 3; i++) {
+        for(int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                boolean[] existsMap = new boolean[10];
+                boolean[] exists = new boolean[9];
                 for (int k = 0; k < 3; k++) {
                     for (int l = 0; l < 3; l++) {
-                        if (board[i * 3 + k][j * 3 + l] == '.') continue;
-                        int value = board[i * 3 + k][j * 3 + l] - '0';
-                        if (existsMap[value]) {
+                        if (board[i * 3 + k][j * 3 + l] == '.')
+                            continue;
+                        if (exists[board[i * 3 + k][j * 3 + l] - '1'])
                             return false;
-                        } else {
-                            existsMap[value] = true;
-                        }
+                        exists[board[i * 3 + k][j * 3 + l] - '1'] = true;
                     }
                 }
             }
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = new char[][]{
+                {'5','3','.','.','7','.','.','.','.'}
+                ,{'6','.','.','1','9','5','.','.','.'}
+                ,{'.','9','8','.','.','.','.','6','.'}
+                ,{'8','.','.','.','6','.','.','.','3'}
+                ,{'4','.','.','8','.','3','.','.','1'}
+                ,{'7','.','.','.','2','.','.','.','6'}
+                ,{'.','6','.','.','.','.','2','8','.'}
+                ,{'.','.','.','4','1','9','.','.','5'}
+                ,{'.','.','.','.','8','.','.','7','9'}
+        };
+        Solution solution = new Solution();
+
+        System.out.println("Input: board = " + Arrays.deepToString(board));
+        System.out.println("Output: " + solution.isValidSudoku(board));
     }
 }

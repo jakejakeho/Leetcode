1f2c272105a795c18af1f41970182d2acd98e504
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:41:05 +0800

    leetcode

---
+++ src/src/com/leetcode/s17/Solution.java
@@ -0,0 +1,28 @@
+package src.com.leetcode.s17;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+
+    Map<String, List<String>> map = Map.of(
+            "2", List.of("a", "b", "c"),
+            "3", List.of("d", "e", "f"),
+            "4", List.of("g", "h", "i"),
+            "5", List.of("j", "k", "l"),
+            "6", List.of("m", "n", "o"),
+            "7", List.of("p", "q", "r", "s"),
+            "8", List.of("t", "u", "v"),
+            "9", List.of("w", "x", "y", "z")
+    );
+
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        for (String str : letterCombinations(digits.substring(1))) {
+            result.add(str);
+        }
+        return result;
+    }
+}

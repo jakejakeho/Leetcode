348e016be51fc0a254521d7d3b6a01f97d1ebdf4
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:41:12 +0800

    LeetCode

--- src/src/com/leetcode/s33/Solution.java
+++ src/src/com/leetcode/s33/Solution.java
@@ -6,43 +6,44 @@ class Solution {
 
     public int search(int[] nums, int target) {
         int minIndex = minIndex(nums);
-        int leftSearch = binarySearch(nums, 0, minIndex - 1, target);
-        int rightSearch = binarySearch(nums, minIndex, nums.length - 1, target);
-        if (leftSearch != -1) {
-            return leftSearch;
-        } else if (rightSearch != -1) {
-            return rightSearch;
+        int k = nums.length - minIndex;
+        if (minIndex == 0)
+            k = 0;
+        if (k == 0) {
+            return binarySearch(nums, 0, nums.length - 1, target);
+        } else {
+            if (target >= nums[0] && target <= nums[nums.length - k - 1]) {
+                return binarySearch(nums, 0, nums.length - k - 1, target);
+            } else {
+                return binarySearch(nums, nums.length - k, nums.length - 1, target);
+            }
         }
-        return -1;
     }
 
     public int minIndex(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            if (nums[left] <= nums[right]) {
-                return left;
-            }
-
-            int mid = (left + right) / 2;
-            if (nums[mid] >= nums[left]) {
-                left = mid + 1;
-            } else {
-                right = mid;
+        int minIndex = -1;
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num < minValue) {
+                minIndex = i;
+                minValue = num;
             }
         }
-        return -1;
+        return minIndex;
     }
 
     public int binarySearch(int[] nums, int start, int end, int target) {
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] == target) {
+        int left = start;
+        int right = end;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == nums[mid]) {
                 return mid;
-            } else if (nums[mid] > target) {
-                end = mid - 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
             } else {
-                start = mid + 1;
+                left = mid + 1;
             }
         }
         return -1;

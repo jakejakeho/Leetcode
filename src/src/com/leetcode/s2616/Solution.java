7ad3b76994b5fa1f7694926069c0604c4ed6395a
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:41:13 +0800

    leetcode

--- src/src/com/leetcode/s2616/Solution.java
+++ src/src/com/leetcode/s2616/Solution.java
@@ -1,50 +1,38 @@
 package src.com.leetcode.s2616;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 
-public class Solution {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.minimizeMax(new int[]{10, 1, 2, 7, 1, 3}, 2));
+class Solution {
+    // Find the number of valid pairs by greedy approach
+    private int countValidPairs(int[] nums, int threshold) {
+        int index = 0, count = 0;
+        while (index < nums.length - 1) {
+            // If a valid pair is found, skip both numbers.
+            if (nums[index + 1] - nums[index] <= threshold) {
+                count++;
+                index++;
+            }
+            index++;
+        }
+        return count;
     }
 
     public int minimizeMax(int[] nums, int p) {
-        PriorityQueue<Pair> pairPriorityQueue = new PriorityQueue<>(Comparator.comparing(Pair::getDifference));
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                pairPriorityQueue.offer(new Pair(nums[i], nums[j]));
+        Arrays.sort(nums);
+        int n = nums.length;
+        int left = 0, right = nums[n - 1] - nums[0];
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // If there are enough pairs, look for a smaller threshold.
+            // Otherwise, look for a larger threshold.
+            if (countValidPairs(nums, mid) >= p) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-
-        List<Pair> pairList = new ArrayList<>(p);
-        while (p > 0) {
-            pairList.add(pairPriorityQueue.poll());
-            p--;
-        }
-
-        int result = 0;
-        for (Pair pair : pairList) {
-            result = Math.max(pair.getDifference(), result);
-        }
-        return result;
-    }
-
-    private static class Pair {
-        int num1;
-        int num2;
-        int difference;
-
-        public Pair(int num1, int num2) {
-            this.num1 = num1;
-            this.num2 = num2;
-            this.difference = Math.abs(num2 - num1);
-        }
-
-        int getDifference() {
-            return difference;
-        }
+        return left;
     }
 }

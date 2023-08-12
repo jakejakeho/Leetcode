bac6e20b0dad8950dc5fc679094d0dbd78095cdc
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:41:10 +0800

    LeetCode

--- src/src/com/leetcode/s46/Solution.java
+++ src/src/com/leetcode/s46/Solution.java
@@ -6,14 +6,13 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.permute(new int[] {0, 1}));
+        System.out.println(solution.permute(new int[] {1, 2, 3}));
     }
 
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> answer = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-        recursive(nums, answer, new ArrayList<>(), used);
-        return answer;
+        List<List<Integer>> result = new ArrayList<>();
+        recursive(nums, result, new ArrayList<>(), new boolean[nums.length]);
+        return result;
     }
 
     public void recursive(int[] nums, List<List<Integer>> answer, List<Integer> currentList, boolean[] used) {
@@ -21,14 +20,13 @@ class Solution {
             answer.add(new ArrayList<>(currentList));
             return;
         }
-
         for (int i = 0; i < nums.length; i++) {
             if (!used[i]) {
                 currentList.add(nums[i]);
                 used[i] = true;
                 recursive(nums, answer, currentList, used);
                 used[i] = false;
-                currentList.remove(Integer.valueOf(nums[i]));
+                currentList.remove(currentList.size() - 1);
             }
         }
     }

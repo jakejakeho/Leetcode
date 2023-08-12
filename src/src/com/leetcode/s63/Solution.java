48e3eae54d0e7c6881cf8a96ce1b7c40ed828445
jakejakeho <mtho@connect.ust.hk>
2023-08-12 20:05:02 +0800

    leetcode

---
+++ src/src/com/leetcode/s63/Solution.java
@@ -0,0 +1,41 @@
+package src.com.leetcode.s63;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.uniquePathsWithObstacles(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+    }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1] == 1) {
+            return 0;
+        }
+        int[][] memo = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        memo[obstacleGrid.length - 1][obstacleGrid[0].length - 1] = 1;
+        return recursive(obstacleGrid, 0, 0, memo);
+    }
+
+    public int recursive(int[][] obstacleGrid, int i, int j, int[][] memo) {
+        if (i >= obstacleGrid.length && j >= obstacleGrid[0].length) {
+            return 0;
+        }
+        if (i <= obstacleGrid.length - 1 && j <= obstacleGrid[0].length - 1) {
+            if (memo[i][j] != -1) {
+                return memo[i][j];
+            } else if (obstacleGrid[i][j] != 1) {
+                int down = recursive(obstacleGrid, i + 1, j, memo);
+                int right = recursive(obstacleGrid, i, j + 1, memo);
+                memo[i][j] = down + right;
+                return memo[i][j];
+            } else {
+                return 0;
+            }
+        }
+        return 0;
+    }
+}

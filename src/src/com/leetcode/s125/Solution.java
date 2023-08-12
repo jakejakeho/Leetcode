6d907a87ac6ee6f0c006f34d2ec51c75e2bb5f1b
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:40:46 +0800

    Easy finished

--- src/src/com/leetcode/s125/Solution.java
+++ src/src/com/leetcode/s125/Solution.java
@@ -4,30 +4,30 @@ import java.util.Objects;
 
 public class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-        while(left < right) {
-            Character leftChar = Character.toLowerCase(s.charAt(left));
-            Character rightChar = Character.toLowerCase(s.charAt(right));
-            if (!isAlphanumeric(leftChar)) {
-                left++;
-                continue;
-            }
-            if (!isAlphanumeric(rightChar)) {
-                right--;
-                continue;
-            }
-            if (!Objects.equals(leftChar, rightChar)) {
+        StringBuilder cleanStr = new StringBuilder();
+        for (int i = 0 ; i < s.length(); i++) {
+            char c = Character.toLowerCase(s.charAt(i));
+            if (isAlphanumeric(c))
+                cleanStr.append(c);
+        }
+        for (int i = 0; i < cleanStr.length() / 2; i++) {
+            char leftChar = Character.toLowerCase(cleanStr.charAt(i));
+            char rightChar = Character.toLowerCase(cleanStr.charAt(cleanStr.length() - 1 - i));
+            if (leftChar != rightChar) {
                 return false;
-            } else {
-                left++;
-                right--;
             }
         }
         return true;
     }
 
-    private boolean isAlphanumeric(Character c) {
-        return (c >= 'a' && c <='z') || (c >= '0' && c <= '9');
+    boolean isAlphanumeric (char c) {
+        return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+    }
+
+    public static void main(String[] args) {
+        String input = "race a car";
+        Solution solution = new Solution();
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + solution.isPalindrome(input));
     }
 }

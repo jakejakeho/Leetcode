98e5663d69de7064788ebd69e80bb848ae0ae9f1
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:41:06 +0800

    leetcode

--- src/src/com/leetcode/s17/Solution.java
+++ src/src/com/leetcode/s17/Solution.java
@@ -17,11 +17,24 @@ class Solution {
             "9", List.of("w", "x", "y", "z")
     );
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.letterCombinations("23"));
+    }
+
 
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
-        for (String str : letterCombinations(digits.substring(1))) {
-            result.add(str);
+        if (digits.length() == 0) return result;
+        String firstDigit = digits.substring(0, 1);
+        for (String str : map.get(firstDigit)) {
+            if (digits.length() > 1) {
+                for (String str2 : letterCombinations(digits.substring(1))) {
+                    result.add(str + str2);
+                }
+            } else {
+                result.add(str);
+            }
         }
         return result;
     }

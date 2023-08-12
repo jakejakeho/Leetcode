99cd09b654e1447b3a7cb80295908c8ef154f509
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:40:46 +0800

    3sum again

--- src/src/com/leetcode/s15/Solution.java
+++ src/src/com/leetcode/s15/Solution.java
@@ -12,28 +12,34 @@ class Solution {
         for (int i = 0; i < nums.length - 2; i++) {
             if (i == 0 || nums[i] != nums[i - 1]) {
                 int target = -nums[i];
-                int left = i + 1;
-                int right = nums.length - 1;
-                while(left < right) {
-                    int sum = nums[left] + nums[right];
-                    if (sum > target) {
-                        right--;
-                    } else if (sum < target) {
-                        left++;
+                int leftPointer = i + 1;
+                int rightPointer = nums.length - 1;
+                while (leftPointer < rightPointer) {
+                    if (nums[leftPointer] + nums[rightPointer] == target) {
+                        result.add(List.of(nums[leftPointer], nums[rightPointer], target));
+                        while (leftPointer < rightPointer && nums[leftPointer] == nums[leftPointer + 1]) {
+                            leftPointer++;
+                        }
+                        while (leftPointer < rightPointer && nums[rightPointer] == nums[rightPointer - 1]) {
+                            rightPointer--;
+                        }
+                        leftPointer++;
+                        rightPointer--;
+                    } else if (nums[leftPointer] + nums[rightPointer] > target) {
+                        rightPointer--;
                     } else {
-                        result.add(List.of(nums[i], nums[left], nums[right]));
-                        while(left < right && nums[left] == nums[left + 1]) {
-                            left++;
-                        }
-                        while(left < right && nums[right] == nums[right - 1]) {
-                            right--;
-                        }
-                        left++;
-                        right--;
+                        leftPointer++;
                     }
                 }
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        Solution solution = new Solution();
+        System.out.println("Input: nums = " + Arrays.toString(nums));
+        System.out.println("Output: " + solution.threeSum(nums));
     }
 }

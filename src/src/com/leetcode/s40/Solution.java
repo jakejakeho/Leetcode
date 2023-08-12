870d883fe872368e7afc9032fcf688d59a9ae5fe
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:41:01 +0800

    leetcode

--- src/src/com/leetcode/s40/Solution.java
+++ src/src/com/leetcode/s40/Solution.java
@@ -2,10 +2,7 @@ package src.com.leetcode.s40;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 class Solution {
 
@@ -15,25 +12,24 @@ class Solution {
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Set<List<Integer>> answer = new HashSet<>();
+        List<List<Integer>> answer = new ArrayList<>();
         Arrays.sort(candidates);
         recursive(candidates, target, answer, new ArrayList<>(), 0);
-        return new ArrayList<>(answer);
+        return answer;
     }
 
-    private void recursive(int[] candidates, int target, Set<List<Integer>> answer, List<Integer> currentList,
-                           int index) {
+    public void recursive(int[] candidates, int target, List<List<Integer>> answer, List<Integer> currentList,
+                          int index) {
         if (target == 0) {
             Collections.sort(currentList);
             answer.add(new ArrayList<>(currentList));
             return;
         }
-
         for (int i = index; i < candidates.length; i++) {
             if (i > index && candidates[i] == candidates[i - 1]) {continue;}
             if (target < candidates[i]) {break;}
 
-            currentList.add(candidates[index]);
+            currentList.add(candidates[i]);
             recursive(candidates, target - candidates[i], answer, currentList, i + 1);
             currentList.remove(currentList.size() - 1);
         }

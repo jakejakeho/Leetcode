8802a2ab50927200536419e96ed2195e02173853
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:41:10 +0800

    LeetCode

--- src/src/com/leetcode/s125/Solution.java
+++ src/src/com/leetcode/s125/Solution.java
@@ -1,26 +1,33 @@
 package src.com.leetcode.s125;
 
-import java.util.Objects;
-
 public class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder cleanStr = new StringBuilder();
-        for (int i = 0 ; i < s.length(); i++) {
-            char c = Character.toLowerCase(s.charAt(i));
-            if (isAlphanumeric(c))
-                cleanStr.append(c);
-        }
-        for (int i = 0; i < cleanStr.length() / 2; i++) {
-            char leftChar = Character.toLowerCase(cleanStr.charAt(i));
-            char rightChar = Character.toLowerCase(cleanStr.charAt(cleanStr.length() - 1 - i));
-            if (leftChar != rightChar) {
+        char[] cleanString = cleanString(s);
+        int left = 0;
+        int right = cleanString.length - 1;
+        while (left < right) {
+            if (cleanString[left] != cleanString[right]) {
                 return false;
             }
+            left++;
+            right--;
         }
         return true;
     }
 
-    boolean isAlphanumeric (char c) {
+    private char[] cleanString(String s) {
+        char[] arr = s.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char lowerCaseChar = Character.toLowerCase(arr[i]);
+            if (isValid(lowerCaseChar)) {
+                stringBuilder.append(lowerCaseChar);
+            }
+        }
+        return stringBuilder.toString().toCharArray();
+    }
+
+    private boolean isValid(char c) {
         return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
     }
 

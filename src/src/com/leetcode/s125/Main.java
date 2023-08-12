acce450c57ffa8e77bec25498303a14ca887369a
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:40:43 +0800

    s125

--- src/src/com/leetcode/s125/Main.java
+++ src/src/com/leetcode/s125/Main.java
@@ -2,7 +2,7 @@ package src.com.leetcode.s125;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "0P";
+        String input = "A man, a plan, a canal: Panama";
         Solution solution = new Solution();
         System.out.println("Input: " + input);
         System.out.println("Output: " + solution.isPalindrome(input));
--- src/src/com/leetcode/s125/Solution.java
+++ src/src/com/leetcode/s125/Solution.java
@@ -1,14 +1,31 @@
 package src.com.leetcode.s125;
 
+import java.util.Objects;
+
 public class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0 ;i < s.length(); i++) {
-            if (Character.isLetterOrDigit(s.charAt(i)) && !Character.isSpaceChar(s.charAt(i)))
-                stringBuilder.append(s.charAt(i));
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right) {
+            Character leftChar = Character.toLowerCase(s.charAt(left));
+            Character rightChar = Character.toLowerCase(s.charAt(right));
+            if (!isAlphanumeric(leftChar)) {
+                left++;
+                continue;
+            }
+            if (!isAlphanumeric(rightChar)) {
+                right--;
+                continue;
+            }
+            if (!Objects.equals(leftChar, rightChar))
+                return false;
+            left++;
+            right--;
         }
-        stringBuilder = new StringBuilder(stringBuilder.toString().toLowerCase());
-        String filtered = stringBuilder.toString();
-        return filtered.equals(new StringBuilder(filtered).reverse().toString());
+        return true;
+    }
+
+    private boolean isAlphanumeric(Character testChar) {
+        return (testChar >= 'a' && testChar <= 'z') || (testChar >= '0' && testChar <= '9');
     }
 }

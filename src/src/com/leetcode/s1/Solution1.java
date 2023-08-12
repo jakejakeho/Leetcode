f0f31164dcbb4e528766ec72ec6f4a0cb5d7c63b
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:40:42 +0800

    s1

--- src/src/com/leetcode/s1/Solution1.java
+++ src/src/com/leetcode/s1/Solution1.java
@@ -1,19 +1,17 @@
 package src.com.leetcode.s1;
 
-
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 public class Solution1 {
+
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                return new int[]{map.get(nums[i]), i};
+            int find = target - nums[i];
+            if (map.containsKey(find)) {
+                return new int[] {i, map.get(find)};
             }
-            map.put((target - nums[i]), i);
+            map.put(nums[i], i);
         }
         return null;
     }

e46d06d92aba7a2ed52bb45f893c7979b6481de0
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:41:12 +0800

    LeetCode

--- src/src/com/leetcode/s74/Solution.java
+++ src/src/com/leetcode/s74/Solution.java
@@ -1,17 +1,21 @@
 package src.com.leetcode.s74;
 
 class Solution {
+
     public boolean searchMatrix(int[][] matrix, int target) {
-        int numOfElements = matrix.length * matrix[0].length;
         int left = 0;
-        int right = numOfElements - 1;
+        int right = matrix.length * matrix[0].length - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (matrix[mid / matrix[0].length][mid % matrix[0].length] == target) {
+            int midI = mid / matrix[0].length;
+            int midJ = mid % matrix[0].length;
+            if (matrix[midI][midJ] == target) {
                 return true;
-            } else if (matrix[mid / matrix[0].length][mid % matrix[0].length] > target) {
+            } else if (matrix[midI][midJ] > target) {
+                // go left
                 right = mid - 1;
             } else {
+                // go right
                 left = mid + 1;
             }
         }
@@ -22,26 +26,31 @@ class Solution {
         Solution solution = new Solution();
 
         // test case 1
-        int[][] matrix = new int[][]{
-                {1, 3, 5, 7},
-                {10, 11, 16, 20},
-                {23, 30, 34, 60},
-        };
+        int[][] matrix = new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60},};
         int target = 3;
         boolean result = solution.searchMatrix(matrix, target);
         boolean excepted = true;
-        System.out.println("Test case 1 result = " + result + " target = " + target + " excepted = " + excepted + " correct? = " + (result == excepted));
-
+        System.out.println(
+            "Test case 1 result = " + result + " target = " + target + " excepted = " + excepted + " correct? = " + (
+                result == excepted));
 
         // test case 2
-        matrix = new int[][]{
-                {1, 3, 5, 7},
-                {10, 11, 16, 20},
-                {23, 30, 34, 60},
-        };
+        matrix = new int[][] {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60},};
         target = 13;
         result = solution.searchMatrix(matrix, target);
         excepted = false;
-        System.out.println("Test case 2 result = " + result + " target = " + target + " excepted = " + excepted + " correct? = " + (result == excepted));
+        System.out.println(
+            "Test case 2 result = " + result + " target = " + target + " excepted = " + excepted + " correct? = " + (
+                result == excepted));
+
+        System.out.println("-------");
+        matrix = new int[][] {{1, 1}};
+        target = 2;
+        result = solution.searchMatrix(matrix, target);
+        excepted = false;
+        System.out.println(
+            "Test case 3 result = " + result + " target = " + target + " excepted = " + excepted + " correct? = " + (
+                result == excepted));
+
     }
 }

72a995a3c70a9a6fdfa3d6e4156f39d642e02c96
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:40:43 +0800

    s128

--- src/src/com/leetcode/s128/Solution.java
+++ src/src/com/leetcode/s128/Solution.java
@@ -5,32 +5,28 @@ import java.util.Map;
 
 public class Solution {
     public int longestConsecutive(int[] nums) {
-        int result = 0;
-        Map<Integer, Boolean> existsMap = new HashMap<>();
-        for (int num: nums) {
-            existsMap.put(num, true);
+        int longest = 0;
+        HashMap<Integer, Boolean> hashMap = new HashMap<>();
+        for (int num : nums) {
+            hashMap.put(num, true);
         }
+
         for (int num: nums) {
-            if (existsMap.get(num).equals(false))
-                continue;
-            existsMap.put(num, false);
             int count = 1;
             int left = num - 1;
-            int right = num + 1;
-            while(existsMap.containsKey(left)) {
-                existsMap.put(left, false);
+            while(hashMap.getOrDefault(left, false)) {
+                hashMap.put(left, false);
                 count++;
                 left--;
             }
-            while(existsMap.containsKey(right)) {
-                existsMap.put(right, false);
+            int right = num + 1;
+            while(hashMap.getOrDefault(right, false)) {
+                hashMap.put(right, false);
                 count++;
                 right++;
             }
-            if (count > result) {
-                result = count;
-            }
+            longest = Math.max(longest, count);
         }
-        return result;
+        return longest;
     }
 }
--- src/src/com/leetcode/s36/Solution.java
+++ src/src/com/leetcode/s36/Solution.java
@@ -5,50 +5,43 @@ import java.util.Map;
 
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for (char[] chars : board) {
-            int[] exists = new int[10];
-            for (int j = 0; j < board[0].length; j++) {
-                if (chars[j] == '.') continue;
-                ;
-                int value = chars[j] - '0';
-                if (exists[value] == 1) {
-                    System.out.println("1");
+        for (int i = 0; i < board.length; i++) {
+            boolean[] existsMap = new boolean[10];
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == '.') continue;
+                int value = board[i][j] - '0';
+                if (existsMap[value]) {
                     return false;
                 } else {
-                    exists[value] = 1;
+                    existsMap[value] = true;
                 }
             }
         }
 
-        for (int i = 0; i < board[0].length; i++) {
-            int[] exists = new int[10];
-            for (char[] chars : board) {
-                if (chars[i] == '.') continue;
-                int value = chars[i] - '0';
-                if (exists[value] == 1) {
-                    System.out.println("2");
+        for (int i = 0; i < board.length; i++) {
+            boolean[] existsMap = new boolean[10];
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][i] == '.') continue;
+                int value = board[j][i] - '0';
+                if (existsMap[value]) {
                     return false;
                 } else {
-                    exists[value] = 1;
+                    existsMap[value] = true;
                 }
             }
         }
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                int[] exists = new int[10];
+                boolean[] existsMap = new boolean[10];
                 for (int k = 0; k < 3; k++) {
                     for (int l = 0; l < 3; l++) {
-                        int x = 3 * i + k;
-                        int y = 3 * j + l;
-                        char c = board[x][y];
-                        if (c == '.') continue;
-                        int value = c - '0';
-                        if (exists[value] == 1) {
-                            System.out.println("3");
+                        if (board[i * 3 + k][j * 3 + l] == '.') continue;
+                        int value = board[i * 3 + k][j * 3 + l] - '0';
+                        if (existsMap[value]) {
                             return false;
                         } else {
-                            exists[value] = 1;
+                            existsMap[value] = true;
                         }
                     }
                 }

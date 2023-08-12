eb0cac2d278b09a6777f687a68bd3ea78dc67a6d
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:40:42 +0800

    S347

--- src/src/com/leetcode/s347/Solution347.java
+++ src/src/com/leetcode/s347/Solution347.java
@@ -4,32 +4,20 @@ import java.util.*;
 
 public class Solution347 {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> count = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            count.merge(num, 1, Integer::sum);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        ArrayList<Integer>[] frequencies = new ArrayList[nums.length + 1];
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            int key = entry.getKey();
-            int frequency = entry.getValue();
-            if (frequencies[frequency] == null) {
-                frequencies[frequency] = new ArrayList<>();
-            }
-            frequencies[frequency].add(key);
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        for (Map.Entry entry : map.entrySet()) {
+            queue.offer(entry);
         }
 
-        int index = 0;
         int[] result = new int[k];
-        for (int frequency = nums.length; frequency > 0; frequency--) {
-            ArrayList<Integer> keys = frequencies[frequency];
-            if (keys != null) {
-                for (Integer key : keys) {
-                    result[index++] = key;
-                    if (index == k)
-                        return result;
-                }
-            }
+        for (int i = 0; i < k; i++) {
+            Map.Entry<Integer, Integer> entry = queue.poll();
+            result[i] = entry.getKey();
         }
         return result;
     }

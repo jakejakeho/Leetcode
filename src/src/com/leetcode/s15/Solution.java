ca7ccccc5edd73050905780360cf8c1661856b67
jakejakeho <mtho@connect.ust.hk>
2023-08-12 10:40:44 +0800

    Array

--- src/src/com/leetcode/s15/Solution.java
+++ src/src/com/leetcode/s15/Solution.java
@@ -11,23 +11,25 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         for (int i = 0; i < nums.length - 2; i++) {
             if (i == 0 || nums[i] != nums[i - 1]) {
-                int target = nums[i];
+                int target = -nums[i];
                 int left = i + 1;
                 int right = nums.length - 1;
-                while (left < right) {
-                    int threeSum = nums[left] + nums[right] + target;
-                    if (threeSum > 0) {
+                while(left < right) {
+                    int sum = nums[left] + nums[right];
+                    if (sum > target) {
                         right--;
-                    } else if (threeSum < 0) {
+                    } else if (sum < target) {
                         left++;
                     } else {
-                        result.add(List.of(nums[left], nums[right], target));
-                        while (left < right && nums[left] == nums[left + 1]) {
+                        result.add(List.of(nums[i], nums[left], nums[right]));
+                        while(left < right && nums[left] == nums[left + 1]) {
                             left++;
                         }
-                        while(left < right && nums[right] != nums[right - 1]) {
+                        while(left < right && nums[right] == nums[right - 1]) {
                             right--;
                         }
+                        left++;
+                        right--;
                     }
                 }
             }
--- src/src/com/leetcode/s167/Solution.java
+++ src/src/com/leetcode/s167/Solution.java
@@ -3,19 +3,18 @@ package src.com.leetcode.s167;
 public class Solution {
 
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
-        while (left < right) {
-            int sum = numbers[left] + numbers[right] - target;
-            if (sum == 0) {
-                return new int[] {left + 1, right + 1};
-            }
-            else if (sum > 0) {
-                right--;
-            } else {
-                left++;
-            }
-        }
-        return new int[]{};
+       int leftPointer = 0;
+       int rightPointer = numbers.length - 1;
+       while (leftPointer < rightPointer) {
+           int sum = numbers[leftPointer] + numbers[rightPointer];
+           if (sum > target) {
+               rightPointer--;
+           } else if (sum < target) {
+               leftPointer++;
+           } else {
+               return new int[]{leftPointer, rightPointer};
+           }
+       }
+       return null;
     }
 }

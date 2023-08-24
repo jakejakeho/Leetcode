7a481252f7eb07dffe51592678681e60fa558c03
jake.ho <jake.ho@okg.com>
2023-08-24 09:00:45 +0800

    LeetCode

--- src/src/com/leetcode/s684/Solution.java
+++ src/src/com/leetcode/s684/Solution.java
@@ -8,35 +8,50 @@ class Solution {
         System.out.println(Arrays.toString(solution.findRedundantConnection(new int[][] {{1, 2}, {1, 3}, {2, 3}})));
     }
 
+    int[] parents;
+
+    int[] rank;
+
+    public boolean union(int n1, int n2) {
+        int p1 = find(n1);
+        int p2 = find(n2);
+        if (p1 == p2) {
+            return false;
+        }
+        if (rank[p1] > rank[p2]) {
+            parents[p2] = p1;
+            rank[p1] += rank[p2];
+        } else {
+            parents[p1] = p2;
+            rank[p2] += rank[p1];
+        }
+        return true;
+    }
+
+    public int find(int n) {
+        int p = parents[n];
+        while (p != parents[p]) {
+            parents[p] = parents[parents[p]];
+            p = parents[p];
+        }
+        return p;
+    }
+
     public int[] findRedundantConnection(int[][] edges) {
-        boolean[][] connected = new boolean[edges.length + 1][edges.length + 1];
+        parents = new int[edges.length + 1];
+        rank = new int[edges.length + 1];
+
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+        }
+        Arrays.fill(rank, 1);
 
         for (int[] edge : edges) {
-            int nodeA = edge[0];
-            int nodeB = edge[1];
-            if (connected[nodeA][nodeB] && connected[nodeB][nodeA]) {
+            boolean union = union(edge[0], edge[1]);
+            if (!union) {
                 return edge;
-            } else {
-                boolean[] currentConnected = new boolean[edges.length + 1];
-                for (int i = 0; i < connected[nodeA].length; i++) {
-                    if (connected[nodeA][i]) {
-                        currentConnected[i] = true;
-                    }
-                }
-                for (int i = 0; i < connected[nodeB].length; i++) {
-                    if (connected[nodeB][i]) {
-                        currentConnected[i] = true;
-                    }
-                }
-                currentConnected[nodeA] = true;
-                currentConnected[nodeB] = true;
-                for (int i = 0; i < currentConnected.length; i++) {
-                    if (currentConnected[i]) {
-                        connected[i] = currentConnected;
-                    }
-                }
             }
         }
-        return new int[] {};
+        return null;
     }
 }

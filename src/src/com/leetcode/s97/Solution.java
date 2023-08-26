6a641a5c848f3771d1d3ffde1527ff1e04c8694a
jakejakeho <mtho@connect.ust.hk>
2023-08-26 14:26:41 +0800

    Leetcode

---
+++ src/src/com/leetcode/s97/Solution.java
@@ -0,0 +1,29 @@
+package src.com.leetcode.s97;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+    }
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[s1.length()][s2.length()] = true;
+        for (int i = dp.length - 1; i >= 0; i--) {
+            for (int j = dp[0].length - 1; j >= 0; j--) {
+                if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j) && dp[i + 1][j]) {
+                    dp[i][j] = true;
+                }
+                if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j) && dp[i][j + 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}

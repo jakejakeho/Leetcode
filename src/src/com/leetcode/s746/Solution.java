a8a6a6d142b6bc095da5ea60b2f2f84eed5820c6
jake.ho <jake.ho@okg.com>
2023-08-27 17:15:13 +0800

    LeetCode

--- src/src/com/leetcode/s746/Solution.java
+++ src/src/com/leetcode/s746/Solution.java
@@ -1,5 +1,7 @@
 package src.com.leetcode.s746;
 
+import java.util.Arrays;
+
 class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -7,6 +9,25 @@ class Solution {
     }
 
     public int minCostClimbingStairs(int[] cost) {
+        int[] minCosts = new int[cost.length ];
+        Arrays.fill(minCosts, -1);
+        return dfs(cost, minCosts, -1);
+    }
 
+    private int dfs(int[] cost, int[] minCosts, int i) {
+        if (i >= cost.length) {
+            return 0;
+        }
+        if (i >= 0 && minCosts[i] != -1) {
+            return minCosts[i];
+        }
+        int currentCost = 0;
+        if (i != -1) {
+            currentCost = cost[i];
+        }
+        currentCost += Math.min(dfs(cost, minCosts, i + 1), dfs(cost, minCosts, i + 2));
+        if (i >= 0)
+            minCosts[i] = currentCost;
+        return currentCost;
     }
 }

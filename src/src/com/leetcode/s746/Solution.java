816cf12d038974e16cf2b0ecc51f578f3b808b93
jakejakeho <mtho@connect.ust.hk>
2023-08-27 11:04:22 +0800

    Leetcode

---
+++ src/src/com/leetcode/s746/Solution.java
@@ -0,0 +1,12 @@
+package src.com.leetcode.s746;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minCostClimbingStairs(new int[]{10, 15, 20}));
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+
+    }
+}

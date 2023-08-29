090ed0daac5f7126c991fbeda054dea7fbf4d53e
jakejakeho <mtho@connect.ust.hk>
2023-08-29 22:43:01 +0800

    Leetcode

--- src/src/com/leetcode/s91/Solution.java
+++ src/src/com/leetcode/s91/Solution.java
@@ -1,46 +1,63 @@
 package src.com.leetcode.s91;
+
 import java.util.Arrays;
 
 class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.numDecodings("12"));
+        System.out.println(solution.numDecodings("22016"));
     }
 
     public int numDecodings(String s) {
         char[] sArr = s.toCharArray();
-        int[] numOfWays = new int[s.length()];
-        Arrays.fill(numOfWays, -1);
-        return numDecodings(sArr, 0, numOfWays);
+        int n = sArr.length;
+        int dp1 = 1;
+        int dp2 = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int dp = isValid(s.charAt(i)) ? dp1 : 0;
+            if ((i + 1) < n && isValid(sArr[i], sArr[i + 1])) {
+                dp += dp2;
+            }
+            dp2 = dp1;
+            dp1 = dp;
+        }
+        return dp1;
     }
 
-    public int numDecodings(char[] sArr, int i, int[] numOfWaysCache) {
-        if (i >= sArr.length) {
-            return 0;
-        }
-        if (numOfWaysCache[i] != -1) {
-            return numOfWaysCache[i];
-        } else {
-            int numOfWays = 0;
-            if (isValid(sArr[i])) {
-                if (i == sArr.length - 1) {
-                    numOfWays++;
-                } else {
-                    numOfWays += numDecodings(sArr, i + 1, numOfWaysCache);
-                }
-            }
-            if (i + 1 < sArr.length && isValid(sArr[i], sArr[i + 1])) {
-                if (i == sArr.length - 2) {
-                    numOfWays++;
-                } else {
-                    numOfWays += numDecodings(sArr, i + 2, numOfWaysCache);
-                }
-            }
-            numOfWaysCache[i] = numOfWays;
-            return numOfWays;
-        }
-    }
+//    public int numDecodings(String s) {
+//        char[] sArr = s.toCharArray();
+//        int[] numOfWays = new int[s.length()];
+//        Arrays.fill(numOfWays, -1);
+//        return numDecodings(sArr, 0, numOfWays);
+//    }
+//
+//    public int numDecodings(char[] sArr, int i, int[] numOfWaysCache) {
+//        if (i >= sArr.length) {
+//            return 0;
+//        }
+//        if (numOfWaysCache[i] != -1) {
+//            return numOfWaysCache[i];
+//        } else {
+//            int numOfWays = 0;
+//            if (isValid(sArr[i])) {
+//                if (i == sArr.length - 1) {
+//                    numOfWays++;
+//                } else {
+//                    numOfWays += numDecodings(sArr, i + 1, numOfWaysCache);
+//                }
+//            }
+//            if (i + 1 < sArr.length && isValid(sArr[i], sArr[i + 1])) {
+//                if (i == sArr.length - 2) {
+//                    numOfWays++;
+//                } else {
+//                    numOfWays += numDecodings(sArr, i + 2, numOfWaysCache);
+//                }
+//            }
+//            numOfWaysCache[i] = numOfWays;
+//            return numOfWays;
+//        }
+//    }
 
     public boolean isValid(char c) {
         return c >= '1' && c <= '9';

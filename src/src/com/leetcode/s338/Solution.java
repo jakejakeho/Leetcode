b02b6bb2640432b0f1f68461695d15b32d9ad310
jakejakeho <mtho@connect.ust.hk>
2023-09-01 09:12:23 +0800

    Leetcode

--- src/src/com/leetcode/s338/Solution.java
+++ src/src/com/leetcode/s338/Solution.java
@@ -1,12 +1,17 @@
 package src.com.leetcode.s338;
+
+import java.util.Arrays;
+
 class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("expected = 0,1,1" + Arrays.toString(solution.countBits(2)));
+    }
+
     public int[] countBits(int n) {
         int[] result = new int[n + 1];
-        for (int j = 0; j <= n; j++) {
-            String bits = Integer.toBinaryString(j);
-            for (int i = result.length - 1, count = 0; i >= 0 && count < bits.length(); i--, count++) {
-                result[i] += bits.charAt(count) - '0';
-            }
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Integer.bitCount(i);
         }
         return result;
     }

1680024260bc703b246cf56ddfc502cee6cd9b18
jakejakeho <mtho@connect.ust.hk>
2023-09-06 23:02:51 +0800

    Leetcode

---
+++ src/src/com/leetcode/s450/Solution.java
@@ -0,0 +1,45 @@
+package src.com.leetcode.s450;
+
+import src.com.utils.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (key > root.val) {
+            // go right
+            root.right = deleteNode(root.right, key);
+            return root;
+        } else if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+            return root;
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            TreeNode current = root.right;
+            while (current.left != null) {
+                current = current.left;
+            }
+            root.val = current.val;
+            root.right = deleteNode(root.right, root.val);
+            return root;
+        }
+    }
+}

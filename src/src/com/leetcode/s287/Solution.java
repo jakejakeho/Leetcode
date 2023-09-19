d9860b47d4c5afca1fda6e32425d41ca7a7d2951
jakejakeho <mtho@connect.ust.hk>
2023-09-19 21:36:48 +0800

    Leetcode

--- src/src/com/leetcode/s287/Solution.java
+++ src/src/com/leetcode/s287/Solution.java
@@ -4,20 +4,15 @@ import java.util.Arrays;
 
 public class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = 0, fast = 0;
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        int slow2 = 0;
-        while(true) {
-            slow = nums[slow];
-            slow2 = nums[slow2];
-            if (slow == slow2) {
-                return slow;
-            }
+        int bits = 0;
+        for (int i = 0; i < nums.length; i++) {
+            bits = bits ^ nums[i];
         }
+
+        for (int i = 1; i < nums.length; i++) {
+            bits = bits ^ i;
+        }
+        return bits;
     }
 
     public static void main(String[] args) {

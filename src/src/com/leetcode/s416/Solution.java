6d9e3e5d363463a4addcef1516c9a265a7a8286b
jakejakeho <mtho@connect.ust.hk>
2023-10-26 08:43:09 +0800

    Leetcode

--- src/src/com/leetcode/s416/Solution.java
+++ src/src/com/leetcode/s416/Solution.java
@@ -1,5 +1,7 @@
 package src.com.leetcode.s416;
 
+import java.util.Arrays;
+
 class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -7,6 +9,21 @@ class Solution {
     }
 
     public boolean canPartition(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) return false;
+
+        int target = sum / 2;
+        boolean[] dp = new boolean[target];
+        dp[0] = true;
+
+        for (int no : nums) {
+            for (int i = target; i >= no; i--) {
+                if (dp[i - no]) {
+                    if (i == target) return true;
+                    dp[i] = true;
+                }
+            }
+        }
         return false;
     }
 }

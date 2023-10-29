699ebc8aff9e162393356ad2d40ad1d7cf698c93
jake.ho <jake.ho@okg.com>
2023-10-29 13:26:51 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s121/Solution.java
+++ src/src/com/leetcode/s121/Solution.java
@@ -1,16 +1,28 @@
 package src.com.leetcode.s121;
 
+import java.util.Arrays;
+
 class Solution {
+
+    public static void main(String[] args) {
+        int[] prices = new int[] {7, 1, 5, 3, 6, 4};
+        Solution solution = new Solution();
+        System.out.println("Input: " + Arrays.toString(prices));
+        System.out.println("Output: " + solution.maxProfit(prices));
+    }
+
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        int buy = Integer.MAX_VALUE;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
+        int maxProfit = 0;
+        int currentProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            int profit = prices[i + 1] - prices[i];
+            if (currentProfit + profit < 0) {
+                currentProfit = 0;
             } else {
-                profit = Math.max((prices[i] - buy), profit);
+                currentProfit += profit;
+                maxProfit = Math.max(maxProfit, currentProfit);
             }
         }
-        return profit;
+        return maxProfit;
     }
 }

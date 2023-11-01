3163a86e8688502a6e27d03e65c68fe6f479e352
jake.ho <jake.ho@okg.com>
2023-11-01 09:22:44 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s3/Solution.java
+++ src/src/com/leetcode/s3/Solution.java
@@ -1,24 +1,34 @@
 package src.com.leetcode.s3;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "dvdf";
+        System.out.println("Input: s = " + s);
+        System.out.println("Output: " + solution.lengthOfLongestSubstring(s));
+    }
+
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) return 0;
-        int longest = 1;
         int leftPointer = 0;
         int rightPointer = 0;
-        Set<Character> existSet = new HashSet<>();
-        while (rightPointer < s.length()) {
-           if (!existSet.contains(s.charAt(rightPointer))) {
-               existSet.add(s.charAt(rightPointer));
-               rightPointer++;
-           } else {
-               existSet.remove(s.charAt(leftPointer));
-               leftPointer++;
-           }
-            longest = Math.max(rightPointer - leftPointer, longest);
+        int longest = 0;
+        Map<Character, Integer> used = new HashMap<>();
+        while (leftPointer <= rightPointer && rightPointer < s.length()) {
+            char c = s.charAt(rightPointer);
+            if (used.containsKey(c)) {
+                leftPointer = used.get(c) + 1;
+                rightPointer = used.get(c) + 1;
+                c = s.charAt(rightPointer);
+                used = new HashMap<>();
+            }
+            used.put(c, rightPointer++);
+            longest = Math.max(longest, rightPointer - leftPointer);
         }
         return longest;
     }

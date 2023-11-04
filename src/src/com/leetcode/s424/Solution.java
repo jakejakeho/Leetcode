5bd1a074509c6dfc541532aea93f208630d9dac6
jakejakeho <mtho@connect.ust.hk>
2023-11-04 16:20:42 +0800

    Leetcode

--- src/src/com/leetcode/s424/Solution.java
+++ src/src/com/leetcode/s424/Solution.java
@@ -4,23 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "AABABBA";
+        int k = 1;
+        System.out.println("Input: s = " + s + ", k = " + k);
+        System.out.println("Output: " + solution.characterReplacement(s, k));
+    }
+
     public int characterReplacement(String s, int k) {
         int longest = 0;
-        int leftPointer = 0;
-        int rightPointer = 0;
-        Map<Character, Integer> count = new HashMap<>();
-        while (rightPointer < s.length()) {
-            count.put(s.charAt(rightPointer), count.getOrDefault(s.charAt(rightPointer), 0) + 1);
-            Integer maxSize = 0;
-            for (Map.Entry<Character, Integer> entry: count.entrySet()) {
-                maxSize = Math.max(entry.getValue(), maxSize);
+        int i = 0;
+        int[] arr = new int[26];
+        int max = 0;
+        for (int j = 0; j < s.length(); j++) {
+            arr[s.charAt(j) - 'A']++;
+            max = Math.max(max, arr[s.charAt(j) - 'A']);
+            if (j - i + 1 - max > k) {
+                arr[s.charAt(i) - 'A']--;
+                i++;
             }
-            while ((rightPointer - leftPointer + 1) - maxSize > k) {
-                count.put(s.charAt(leftPointer), count.get(s.charAt(leftPointer) )- 1);
-                leftPointer++;
-            }
-            longest = Math.max(longest, rightPointer - leftPointer + 1);
-            rightPointer++;
+            longest = Math.max(longest, j - i + 1);
         }
         return longest;
     }

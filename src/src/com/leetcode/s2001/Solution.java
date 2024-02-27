f89af03d1b6e27d61666f29a726dcbee264bba05
jake.ho <jake.ho@okg.com>
2024-02-27 13:45:49 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s2001/Solution.java
+++ src/src/com/leetcode/s2001/Solution.java
@@ -1,25 +1,26 @@
 package src.com.leetcode.s2001;
 
 import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.interchangeableRectangles(new int[][]{{4, 8}, {3, 6}, {10, 20}, {15, 30}}));
+        System.out.println(solution.interchangeableRectangles(new int[][] {{4, 8}, {3, 6}, {10, 20}, {15, 30}}));
     }
 
     public long interchangeableRectangles(int[][] rectangles) {
-        HashMap<Double, Integer> count = new HashMap<>();
-        for (int[] rectangle : rectangles) {
-            Double hashKey = (rectangle[0] + 0.0) / rectangle[1];
-            count.putIfAbsent(hashKey, 0);
-            count.computeIfPresent(hashKey, (key, value) -> ++value);
-        }
-
+        Map<Double, Long> map = new HashMap<>();
         long result = 0;
-        for (long currentCount : count.values()) {
-            result += (currentCount * (currentCount - 1)) / 2;
+        for (int[] rectangle : rectangles) {
+            double width = rectangle[0];
+            double height = rectangle[1];
+            double ratio = width / height;
+            if (map.containsKey(ratio)) {
+                result += map.get(ratio);
+            }
+            map.put(ratio, map.getOrDefault(ratio, 0L) + 1);
         }
         return result;
     }

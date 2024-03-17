50845de076216489b7da59a8db09d74e224b6894
jakejakeho <mtho@connect.ust.hk>
2024-03-17 13:04:43 +0800

    Leetcode

--- src/src/com/leetcode/s57/Solution.java
+++ src/src/com/leetcode/s57/Solution.java
@@ -12,51 +12,59 @@ class Solution {
     }
 
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list = new ArrayList<>(intervals.length);
-        Integer min = newInterval[0];
-        Integer max = newInterval[1];
-        boolean needCombine = false;
-        for (int[] interval : intervals) {
-            if (overlap(interval, newInterval)) {
-                needCombine = true;
-                if (min == null)
-                    min = Math.min(interval[0], newInterval[0]);
-                else
-                    min = Math.min(min, Math.min(interval[0], newInterval[0]));
-                if (max == null)
-                    max = Math.max(interval[1], newInterval[1]);
-                else
-                    max = Math.max(max, Math.max(interval[1], newInterval[1]));
+        if (intervals.length == 0) {
+            return new int[][]{newInterval};
+        }
+        List<int[]> resultList = new ArrayList<>();
+        boolean isOverlap = false;
+        for (int i = 0; i < intervals.length; i++) {
+            if (isOverlap(intervals[i], newInterval)) {
+                newInterval = new int[]{Math.min(intervals[i][0], newInterval[0]), Math.max(intervals[i][1], newInterval[1])};
+                isOverlap = true;
             } else {
-                if (needCombine && min != null) {
-                    list.add(new int[]{min, max});
-                    min = null;
-                    max = null;
+                if (isOverlap) {
+                    resultList.add(newInterval);
+                    newInterval = null;
+                    isOverlap = false;
                 }
-                list.add(interval);
+                resultList.add(intervals[i]);
             }
         }
-        if (min != null) {
-            list.add(new int[]{min, max});
+        if (isOverlap) {
+            resultList.add(newInterval);
+        } else if (newInterval != null) {
+            for (int i = 0; i < resultList.size(); i++) {
+                int newStart = newInterval[0];
+                int oldStart = resultList.get(i)[0];
+                if (newStart < oldStart) {
+                    resultList.add(i, newInterval);
+                    newInterval = null;
+                    break;
+                }
+            }
+            if (newInterval != null) {
+                resultList.add(newInterval);
+            }
         }
-        list.sort(Comparator.comparingInt(a -> a[1]));
-        int[][] result = new int[list.size()][2];
-        int i = 0;
-        for (int[] interval : list) {
-            result[i++] = interval;
+        int[][] result = new int[resultList.size()][2];
+        for (int i = 0; i < resultList.size(); i++) {
+            int[] interval = resultList.get(i);
+            result[i][0] = interval[0];
+            result[i][1] = interval[1];
         }
         return result;
     }
 
-    private boolean overlap(int[] interval, int[] newInterval) {
-        int start1 = interval[0];
-        int end1 = interval[1];
-        int start2 = newInterval[0];
-        int end2 = newInterval[1];
-        boolean isOverlap = start2 <= end1 && start2 >= start1 ||
-                end2 >= start1 && end2 <= end1 ||
-                start1 <= end2 && start1 >= start2 ||
-                end1 >= start2 && end1 <= end2;
-        return isOverlap;
+    public boolean isOverlap(int[] oldInterval, int[] newInterval) {
+        if (newInterval == null) {
+            return false;
+        }
+        int newStartTime = newInterval[0];
+        int newEndTime = newInterval[1];
+        int oldStartTime = oldInterval[0];
+        int oldEndTime = oldInterval[1];
+        return (newStartTime >= oldStartTime && newStartTime <= oldEndTime) || (newEndTime >= oldStartTime && newEndTime <= oldEndTime)
+                || (oldStartTime >= newStartTime && oldStartTime <= newEndTime) || (oldEndTime >= newStartTime && oldEndTime <= newEndTime);
     }
+
 }

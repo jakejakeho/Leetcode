8f63b7767a46c123ea96e3f66f4ce88a712375e4
jakejakeho <mtho@connect.ust.hk>
2024-03-17 09:59:15 +0800

    Leetcode

--- src/src/com/leetcode/s238/Solution.java
+++ src/src/com/leetcode/s238/Solution.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
-        int prefix = 1;
-        for (int i = 0; i < nums.length; i++) {
-            result[i] = prefix;
-            prefix *= nums[i];
+        result[0] = 1;
+        int pre = 1;
+        for (int i = 1; i < nums.length; i++) {
+            pre *= nums[i - 1];
+            result[i] = pre;
         }
-        int suffix = 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            result[i] *= suffix;
-            suffix *= nums[i];
+        int suf = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suf *= nums[i + 1];
+            result[i] *= suf;
         }
         return result;
     }

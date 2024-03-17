25b89c56070905857555726b7e5c2e9f2be57e12
jakejakeho <mtho@connect.ust.hk>
2024-03-17 10:23:19 +0800

    Leetcode

--- src/src/com/leetcode/s930/Solution.java
+++ src/src/com/leetcode/s930/Solution.java
@@ -2,25 +2,27 @@ package src.com.leetcode.s930;
 
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int num = 0;
-        for (int length = Math.max(1, goal); length <= nums.length; length++) {
-            int sum = 0;
-            for (int i = 0; i < length; i++) {
-                sum += nums[i];
-            }
-            if (sum == goal) {
-                num++;
-            }
-            for (int i = 0; i < nums.length - length; i++) {
-                // shift right
-                sum -= nums[i];
-                sum += nums[i + length];
-                if (sum == goal) {
-                    num++;
-                }
-            }
+        return helper(nums, goal) - helper(nums, goal - 1);
+    }
+
+    // count sum of subarrs where curSum <= x
+    public int helper(int[] nums, int goal) {
+        if (goal < 0) {
+            return 0;
         }
-        return num;
+
+        int result = 0;
+        int leftPointer = 0;
+        int current = 0;
+        for (int rightPointer = 0; rightPointer < nums.length; rightPointer++) {
+            current += nums[rightPointer];
+            while (current > goal) {
+                current -= nums[leftPointer];
+                leftPointer++;
+            }
+            result += (rightPointer - leftPointer) + 1;
+        }
+        return result;
     }
 
     public static void main(String[] args) {

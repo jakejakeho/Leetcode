ce25e74d9c5417d70bd01f383544a8aa55f6ec7c
jakejakeho <mtho@connect.ust.hk>
2024-03-21 08:27:12 +0800

    Leetcode

--- src/src/com/leetcode/s206/Solution.java
+++ src/src/com/leetcode/s206/Solution.java
@@ -1,58 +1,37 @@
 package src.com.leetcode.s206;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Solution {
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null) return null;
-        ListNode pointer = head;
-        ListNode lastNode = null;
-        do {
-            ListNode nextNode = pointer.next;
-            pointer.next = lastNode;
-            lastNode = pointer;
-            pointer = nextNode;
-        } while (pointer != null);
+        ListNode lastNode = head;
+        ListNode current = head.next;
+        lastNode.next = null;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = lastNode;
+            lastNode = current;
+            current = nextNode;
+        }
         return lastNode;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-
-        ListNode list;
-        ListNode output;
-        /*
-         * Input: head = [1,2,3,4,5]
-         * Output: [5,4,3,2,1]
-         */
-        list = new ListNode(List.of(1, 2, 3, 4, 5));
-        System.out.println("Input: ");
-        list.print();
-        output = solution.reverseList(list);
-        System.out.println("Output: ");
-        output.print();
-
-        /*
-         * Input: head = [1,2]
-         * Output: [2,1]
-         */
-        list = new ListNode(List.of(1, 2));
-        System.out.println("Input: ");
-        list.print();
-        output = solution.reverseList(list);
-        System.out.println("Output: ");
-        output.print();
-
-        /*
-         * Input: head = []
-         * Output: []
-         */
-        list = new ListNode(new ArrayList<>());
-        System.out.println("Input: ");
-        list.print();
-        output = solution.reverseList(list);
-        System.out.println("Output: ");
-        output.print();
+        ListNode listNode =  new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+        System.out.println(solution.reverseList(listNode));
     }
 }

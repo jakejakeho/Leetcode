0e8f2f22a513c39abfeca9501c3850d68e17eba5
jakejakeho <mtho@connect.ust.hk>
2024-03-24 09:21:58 +0800

    Leetcode

--- src/src/com/leetcode/s287/Solution.java
+++ src/src/com/leetcode/s287/Solution.java
@@ -1,32 +1,29 @@
 package src.com.leetcode.s287;
 
-import java.util.Arrays;
-
-public class Solution {
+class Solution {
     public int findDuplicate(int[] nums) {
-        int tortoise = 0, hare = 0;
-        do {
-            tortoise = nums[tortoise];
-            hare = nums[nums[hare]];
-        } while(nums[tortoise] != nums[hare]);
+        int slowPointer = nums[0];
+        int fastPointer = nums[0];
 
-        int p1 = 0, p2 = tortoise;
-        while (nums[p1] != nums[p2]) {
-            p1 = nums[p1];
-            p2 = nums[p2];
+        do {
+            slowPointer = nums[slowPointer];
+            fastPointer = nums[nums[fastPointer]];
+        } while (slowPointer != fastPointer);
+
+        slowPointer = nums[0];
+
+        while (slowPointer != fastPointer) {
+            slowPointer = nums[slowPointer];
+            fastPointer = nums[fastPointer];
         }
-        return nums[p1];
+
+        return slowPointer;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums;
-        /**
-         * Input: nums = [1,3,4,2,2]
-         * Output: 2
-         */
-
         nums = new int[]{1, 3, 4, 2, 2};
-        System.out.println("Output = " + solution.findDuplicate(nums));
+        System.out.println(solution.findDuplicate(nums));
     }
 }

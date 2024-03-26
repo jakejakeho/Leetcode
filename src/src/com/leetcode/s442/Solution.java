2455d6c852771615c50a1b0d6aa93a2be22300e2
jakejakeho <mtho@connect.ust.hk>
2024-03-26 16:56:21 +0800

    Leetcode

---
+++ src/src/com/leetcode/s442/Solution.java
@@ -0,0 +1,24 @@
+package src.com.leetcode.s442;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                result.add(Math.abs(nums[i]));
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findDuplicates(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
+    }
+}

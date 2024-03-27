806f0326ba9f0cd2129dda99fbbbf3b974072b20
jakejakeho <mtho@connect.ust.hk>
2024-03-27 08:38:12 +0800

    Leetcode

---
+++ src/src/com/leetcode/s713/Solution.java
@@ -0,0 +1,12 @@
+package src.com.leetcode.s713;
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100));
+    }
+}

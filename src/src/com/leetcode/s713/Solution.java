785a44d1a4344045bd76ee66b081449dcd07f322
jakejakeho <mtho@connect.ust.hk>
2024-03-27 08:41:59 +0800

    Leetcode

--- src/src/com/leetcode/s713/Solution.java
+++ src/src/com/leetcode/s713/Solution.java
@@ -2,11 +2,24 @@ package src.com.leetcode.s713;
 
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int product = 1;
+            for (int j = 0; j < nums.length; j++) {
+                product *= nums[j];
+                if (product < k) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        System.out.println(solution.numSubarrayProductLessThanK(new int[]{1, 1, 1}, 2));
         System.out.println(solution.numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100));
     }
 }

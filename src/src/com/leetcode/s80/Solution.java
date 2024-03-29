4587331db3b9b409224d3a63eb1fec2fc0dbaf9e
jakejakeho <mtho@connect.ust.hk>
2024-03-29 20:02:52 +0800

    Leetcode

---
+++ src/src/com/leetcode/s80/Solution.java
@@ -0,0 +1,27 @@
+package src.com.leetcode.s80;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int left = 0;
+        int right = 0;
+        int lastNum = Integer.MIN_VALUE;
+        int duplicateCount = 0;
+
+        while (right < nums.length) {
+            if (nums[right] == lastNum && duplicateCount <= 1) {
+                nums[left] = nums[right];
+                left++;
+                duplicateCount++;
+            }
+            if (nums[right] != lastNum) {
+                lastNum = nums[right];
+                duplicateCount = 0;
+                nums[left] = nums[right];
+                left++;
+                duplicateCount++;
+            }
+            right++;
+        }
+        return left;
+    }
+}

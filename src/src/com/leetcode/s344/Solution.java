616906510a5552c1fa683079f034c1c123ce638e
jakejakeho <mtho@connect.ust.hk>
2024-03-29 17:49:17 +0800

    Leetcode

---
+++ src/src/com/leetcode/s344/Solution.java
@@ -0,0 +1,15 @@
+package src.com.leetcode.s344;
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+}

3954c4119b98da0a8c751c77b6f0f3d9feace5f5
jakejakeho <mtho@connect.ust.hk>
2024-04-02 08:23:34 +0800

    Leetcode

--- src/src/com/leetcode/s205/Solution.java
+++ src/src/com/leetcode/s205/Solution.java
@@ -1,30 +1,22 @@
 package src.com.leetcode.s205;
 
 import java.util.HashMap;
-import java.util.Objects;
+import java.util.Map;
 
-public class Solution {
+class Solution {
     public boolean isIsomorphic(String s, String t) {
-        String newS = assignStr(s);
-        String newT = assignStr(t);
-        return Objects.equals(newS, newT);
-    }
-
-    public String assignStr(String s) {
-        char[] chars = s.toCharArray();
-        StringBuilder sb = new StringBuilder();
-        char used = 'a';
-        HashMap<Character, Character> hashMap = new HashMap<>();
-        for (char c : chars) {
-            hashMap.putIfAbsent(c, used);
-            used++;
-            sb.append(hashMap.get(c));
+        Map<Character, Character> sMap = new HashMap<>();
+        Map<Character, Character> tMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            if (sMap.containsKey(sChar) && sMap.get(sChar) != tChar || tMap.containsKey(tChar) && tMap.get(tChar) != sChar) {
+                return false;
+            } else {
+                sMap.put(sChar, tChar);
+                tMap.put(tChar, sChar);
+            }
         }
-        return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.isIsomorphic("egg", "add");
+        return true;
     }
 }

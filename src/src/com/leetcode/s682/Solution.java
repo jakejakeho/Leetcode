8b61439a87a42f0cbf6d6aa2c4aafc7412e840bc
jakejakeho <mtho@connect.ust.hk>
2024-04-03 21:57:00 +0800

    Leetcode

--- src/src/com/leetcode/s682/Solution.java
+++
@@ -1,7 +0,0 @@
-package src.com.leetcode.s682;
-
-class Solution {
-    public int calPoints(String[] operations) {
-        
-    }
-}
--- src/src/com/leetcode/s79/Solution.java
+++ src/src/com/leetcode/s79/Solution.java
@@ -1,60 +1,72 @@
 package src.com.leetcode.s79;
-class Solution {
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.exist(new char[][]{{'A','B','C','E'}, {'S','F','C','S'}, {'A','D','E','E'}}, "ABCCED"));
-    }
+class Solution {
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 boolean[][] visited = new boolean[board.length][board[0].length];
-                if (recursive(board, word, i, j, 0, visited))
+                if (dfs(board, word, i, j, 0, visited)) {
                     return true;
+                }
             }
         }
         return false;
     }
 
-    private boolean recursive(char[][] board, String word, int currentI, int currentJ, int matchIndex,
-                              boolean[][] visited) {
-        char currentChar = word.charAt(matchIndex);
-        boolean charMatch = board[currentI][currentJ] == currentChar;
-        if (charMatch && matchIndex == word.length() - 1) {
+    private boolean dfs(char[][] board, String word, int i, int j, int wordIndex, boolean[][] visited) {
+        if (wordIndex < word.length() && board[i][j] != word.charAt(wordIndex)) {
+            return false;
+        }
+
+        visited[i][j] = true;
+        if (wordIndex + 1 == word.length()) {
             return true;
         }
-        if (charMatch) {
-            visited[currentI][currentJ] = true;
-            boolean exist = false;
-            // go up
-            if (currentI - 1 >= 0 && !visited[currentI - 1][currentJ])
-                if (recursive(board, word, currentI - 1, currentJ, matchIndex + 1, visited))
-                    exist = true;
-                else
-                    visited[currentI - 1][currentJ] = false;
 
-            // go down
-            if (currentI + 1 < board.length && !visited[currentI + 1][currentJ])
-                if (recursive(board, word, currentI + 1, currentJ, matchIndex + 1, visited))
-                    exist = true;
-                else
-                    visited[currentI + 1][currentJ] = false;
+        // go up
+        if ((i - 1) >= 0 && !visited[i - 1][j]) {
+            if (dfs(board, word, i - 1, j, wordIndex + 1, visited)) {
+                return true;
+            } else {
+                visited[i - 1][j] = false;
+            }
+        }
 
-            // go left
-            if (currentJ - 1 >= 0 && !visited[currentI][currentJ - 1])
-                if (recursive(board, word, currentI, currentJ - 1, matchIndex + 1, visited))
-                    exist = true;
-                else
-                    visited[currentI][currentJ - 1] = false;
+        // go down
+        if ((i + 1) < board.length && !visited[i + 1][j]) {
+            if (dfs(board, word, i + 1, j, wordIndex + 1, visited)) {
+                return true;
+            } else {
+                visited[i + 1][j] = false;
+            }
+        }
 
-            // go right
-            if (currentJ + 1 < board[0].length && !visited[currentI][currentJ + 1])
-                if (recursive(board, word, currentI, currentJ + 1, matchIndex + 1, visited))
-                    exist = true;
-                else
-                    visited[currentI][currentJ + 1] = false;
-            return exist;
+        // go left
+        if ((j - 1) >= 0 && !visited[i][j - 1]) {
+            if (dfs(board, word, i, j - 1, wordIndex + 1, visited)) {
+                return true;
+            } else {
+                visited[i][j - 1] = false;
+            }
+        }
+
+        // go right
+        if ((j + 1) < board[0].length && !visited[i][j + 1]) {
+            if (dfs(board, word, i, j + 1, wordIndex + 1, visited)) {
+                return true;
+            } else {
+                visited[i][j + 1] = false;
+            }
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.exist(new char[][]{
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'E', 'S'},
+                {'A', 'D', 'E', 'E'},
+        }, "ABCESEEEFS"));
     }
 }

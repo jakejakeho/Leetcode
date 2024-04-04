3e1c6eb938edaf1798b3da830e6f195d1f2e4a4a
jakejakeho <mtho@connect.ust.hk>
2024-04-04 09:55:26 +0800

    Leetcode

--- src/src/com/leetcode/s225/MyStack.java
+++ src/src/com/leetcode/s225/MyStack.java
@@ -1,13 +1,15 @@
 package src.com.leetcode.s225;
+
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 class MyStack {
 
-    Queue<Integer> queue = new LinkedList<>();
+    Queue<Integer> queue;
 
     public MyStack() {
-
+        queue = new LinkedList<>();
     }
 
     public void push(int x) {
@@ -15,36 +17,41 @@ class MyStack {
     }
 
     public int pop() {
-        Queue<Integer> queue1 = new LinkedList<>();
-        int popVal = 0;
-        while(!queue.isEmpty()) {
-            if (queue.size() == 1) {
-                popVal = queue.poll();
-                break;
-            } else {
-                queue1.offer(queue.poll());
-            }
-        }
-        queue = queue1;
+        Queue<Integer> tmp = new LinkedList<>();
+        while (queue.size() > 1)
+            tmp.offer(queue.poll());
+        int popVal = queue.poll();
+        queue = tmp;
         return popVal;
     }
 
     public int top() {
+        Queue<Integer> tmp = new LinkedList<>();
         int top = 0;
-        Queue<Integer> temp = new LinkedList<>();
         while (!queue.isEmpty()) {
             if (queue.size() == 1) {
-                top = queue.peek();
-            }
-            temp.offer(queue.poll());
+                top = queue.poll();
+                tmp.offer(top);
+            } else
+                tmp.offer(queue.poll());
         }
-        queue = temp;
+        queue = tmp;
         return top;
     }
 
     public boolean empty() {
         return queue.isEmpty();
     }
+
+    /**
+     * Your MyStack object will be instantiated and called as such:
+     * MyStack obj = new MyStack();
+     * obj.push(x);
+     * int param_2 = obj.pop();
+     * int param_3 = obj.top();
+     * boolean param_4 = obj.empty();
+     */
+
 
     public static void main(String[] args) {
         MyStack myStack = new MyStack();

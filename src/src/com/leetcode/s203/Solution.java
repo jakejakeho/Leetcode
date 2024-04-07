eff3d3580afeba889037435f166a92e42e05d74d
jakejakeho <mtho@connect.ust.hk>
2024-04-07 11:05:55 +0800

    Leetcode

---
+++ src/src/com/leetcode/s203/Solution.java
@@ -0,0 +1,43 @@
+package src.com.leetcode.s203;
+
+import src.com.leetcode.s206.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode lastNode = dummy;
+        ListNode pointer = head;
+        while (pointer != null) {
+            if (pointer.val == val) {
+                lastNode.next = pointer.next;
+            } else {
+                lastNode = pointer;
+            }
+            pointer = pointer.next;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(6);
+        l1.next.next.next = new ListNode(3);
+        l1.next.next.next.next = new ListNode(4);
+        l1.next.next.next.next.next = new ListNode(5);
+        l1.next.next.next.next.next.next = new ListNode(6);
+        System.out.println(solution.removeElements(l1, 6));
+    }
+}

9ff4827ea9a46a7090ae393e14e76ba3ae5a90f9
jakejakeho <mtho@connect.ust.hk>
2024-04-07 10:27:03 +0800

    Leetcode

--- src/src/com/leetcode/s977/Solution.java
+++ src/src/com/leetcode/s977/Solution.java
@@ -1,39 +1,44 @@
 package src.com.leetcode.s977;
 
+import java.util.Arrays;
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] result = new int[nums.length];
-        int rightPointer = 0;
-        while (rightPointer < nums.length && nums[rightPointer] < 0) {
-            rightPointer++;
+        if (nums.length == 1) {
+            nums[0] = nums[0] * nums[0];
+            return nums;
         }
+        int firstPositiveIndex = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            if (firstPositiveIndex == nums.length && nums[i] >= 0) {
+                firstPositiveIndex = i;
+            }
+            nums[i] = nums[i] * nums[i];
+        }
+        int left = firstPositiveIndex - 1;
+        int right = firstPositiveIndex;
+        int[] sorted = new int[nums.length];
+        int index = 0;
+        while (left >= 0 || right < nums.length) {
+            boolean leftNotDone = left >= 0;
+            boolean rightNotDone = right < nums.length;
+            if (!leftNotDone) {
+                sorted[index++] = nums[right++];
+            } else if (!rightNotDone) {
+                sorted[index++] = nums[left--];
+            } else {
+                if (nums[left] <= nums[right]) {
+                    sorted[index++] = nums[left--];
+                } else {
+                    sorted[index++] = nums[right++];
+                }
+            }
+        }
+        return sorted;
+    }
 
-        int leftPointer = rightPointer - 1;
-        int currentIndex = 0;
-        while (currentIndex < nums.length) {
-            int leftVal;
-            int rightVal;
-            if (leftPointer >= 0) {
-                leftVal = nums[leftPointer];
-            } else {
-                result[currentIndex++] = (int)Math.pow(nums[rightPointer++], 2);
-                continue;
-            }
-            if (rightPointer < nums.length) {
-                rightVal = nums[rightPointer];
-            } else {
-                result[currentIndex++] = (int)Math.pow(nums[leftPointer--], 2);
-                continue;
-            }
-            
-            if (Math.abs(leftVal) < Math.abs(rightVal)) {
-                result[currentIndex++] = (int)Math.pow(leftVal, 2);
-                leftPointer--;
-            } else {
-                result[currentIndex++] = (int)Math.pow(rightVal, 2);
-                rightPointer++;
-            }
-        }
-        return result;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.sortedSquares(new int[]{-4, -1, 0, 3, 10})));
     }
 }

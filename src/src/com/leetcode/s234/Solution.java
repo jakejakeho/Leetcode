9374e3f32502c1ee2eaa9112133e9d183a840b19
jakejakeho <mtho@connect.ust.hk>
2024-04-07 11:00:11 +0800

    Leetcode

--- src/src/com/leetcode/s234/Solution.java
+++ src/src/com/leetcode/s234/Solution.java
@@ -2,77 +2,60 @@ package src.com.leetcode.s234;
 
 import src.com.leetcode.s206.ListNode;
 
-import java.util.Objects;
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        head.next.next.next.next.next = new ListNode(5);
-        head.next.next.next.next.next.next = new ListNode(4);
-        head.next.next.next.next.next.next.next = new ListNode(3);
-        head.next.next.next.next.next.next.next.next = new ListNode(2);
-        head.next.next.next.next.next.next.next.next.next = new ListNode(1);
-//        ListNode head = new ListNode(1);
-//        head.next = new ListNode(2);
-//        head.next.next = new ListNode(1);
-        System.out.println(solution.isPalindrome(head));
-    }
     public boolean isPalindrome(ListNode head) {
-        int size = size(head);
-        ListNode beforeHalf = null;
-        ListNode afterHalf = head;
-        for (int i = 0; i < size / 2; i++) {
-            beforeHalf = afterHalf;
-            afterHalf = afterHalf.next;
-        }
-        // is odd
-        if (size % 2 == 1) {
-            afterHalf = afterHalf.next;
-        }
-
         ListNode lastNode = null;
-        ListNode current = head;
-        while (current != afterHalf) {
-            ListNode nextNode = current.next;
-            current.next = lastNode;
-            lastNode = current;
-            current = nextNode;
-        }
-
-
-        while (beforeHalf != null && afterHalf != null) {
-            if (!Objects.equals(beforeHalf.val, afterHalf.val)) {
-                return false;
+        ListNode slowPointer = head;
+        ListNode fastPointer = slowPointer.next;
+        ListNode firstHalf = null;
+        ListNode secondHalf = null;
+        int numOfElements = 1;
+        while (fastPointer != null) {
+            ListNode nextNode = slowPointer.next;
+            slowPointer.next = lastNode;
+            lastNode = slowPointer;
+            firstHalf = lastNode;
+            secondHalf = nextNode;
+            slowPointer = nextNode;
+            fastPointer = fastPointer.next;
+            numOfElements++;
+            if (fastPointer != null) {
+                fastPointer = fastPointer.next;
+                numOfElements++;
             }
-            beforeHalf = beforeHalf.next;
-            afterHalf = afterHalf.next;
+        }
+        if (numOfElements == 1) {
+            return true;
+        }
+        if (numOfElements % 2 == 1) {
+            secondHalf = secondHalf.next;
+        }
+        ListNode p1 = firstHalf;
+        ListNode p2 = secondHalf;
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val)
+                return false;
+            p1 = p1.next;
+            p2 = p2.next;
         }
         return true;
     }
 
-    public int size(ListNode head) {
-        int size = 0;
-        ListNode current = head;
-        while (current != null) {
-            current = current.next;
-            size++;
-        }
-        return size;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(1);
+        System.out.println(solution.isPalindrome(l1));
     }
 }

b8792500bf29b265b777cf3a4c4388c41bf03213
jakejakeho <mtho@connect.ust.hk>
2024-04-07 16:12:31 +0800

    Leetcode

---
+++ src/src/com/leetcode/s94/Solution.java
@@ -0,0 +1,49 @@
+package src.com.leetcode.s94;
+
+import src.com.leetcode.s206.ListNode;
+import src.com.utils.TreeNode;
+
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                TreeNode node = stack.peek();
+                stack.pop();
+                list.add(node.val);
+                current = node.right;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(solution.inorderTraversal(root));
+    }
+}

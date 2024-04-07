4082e8eea2327a08c6a855a92d3ad610c574464f
jakejakeho <mtho@connect.ust.hk>
2024-04-07 08:42:29 +0800

    Leetcode

--- src/src/com/leetcode/s678/Solution.java
+++ src/src/com/leetcode/s678/Solution.java
@@ -1,34 +1,35 @@
 package src.com.leetcode.s678;
 
-
 class Solution {
+    public boolean checkValidString(String s) {
+        int numOfOpenParenthesisMax = 0;
+        int numOfOpenParenthesisMin = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                numOfOpenParenthesisMax++;
+                numOfOpenParenthesisMin++;
+            } else if (s.charAt(i) == ')') {
+                numOfOpenParenthesisMax--;
+                numOfOpenParenthesisMin--;
+            } else {
+                numOfOpenParenthesisMax++;
+                numOfOpenParenthesisMin--;
+            }
+            if (numOfOpenParenthesisMax < 0) {
+                return false;
+            }
+            if (numOfOpenParenthesisMin < 0) {
+                numOfOpenParenthesisMin = 0;
+            }
+        }
+        return numOfOpenParenthesisMin == 0;
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        System.out.println(solution.checkValidString("()"));
+        System.out.println(solution.checkValidString("(*)"));
+        System.out.println(solution.checkValidString("(*))"));
         System.out.println(solution.checkValidString("((((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)()"));
-    }
-
-    public boolean checkValidString(String s) {
-        int leftMin = 0;
-        int leftMax = 0;
-        char[] charArr = s.toCharArray();
-        for (char c : charArr) {
-            if (c == '(') {
-                leftMin++;
-                leftMax++;
-            } else if (c == ')') {
-                leftMin--;
-                leftMax--;
-            } else {
-                leftMin--;
-                leftMax++;
-            }
-            if (leftMax < 0) {
-                return false;
-            }
-            if (leftMin < 0)
-                leftMin = 0;
-        }
-        return leftMin == 0;
     }
 }

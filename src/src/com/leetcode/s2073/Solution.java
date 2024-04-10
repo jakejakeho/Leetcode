d1b5753893c719f3344a92cdfeb6cffef2a5f207
jake.ho <jake.ho@okg.com>
2024-04-10 10:18:19 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s2073/Solution.java
+++ src/src/com/leetcode/s2073/Solution.java
@@ -1,23 +1,26 @@
 package src.com.leetcode.s2073;
 
 class Solution {
+
     public int timeRequiredToBuy(int[] tickets, int k) {
         int total = 0;
         int target = tickets[k];
         for (int i = 0; i < tickets.length; i++) {
-            if (i > k && tickets[i] >= target)
-                total += Math.min(target - 1, tickets[i]);
-            else
+            if (i > k && tickets[i] >= target) {total += Math.min(target - 1, tickets[i]);} else {
                 total += Math.min(target, tickets[i]);
+            }
         }
         return total;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.timeRequiredToBuy(new int[]{2, 3, 2}, 2));
-        System.out.println(solution.timeRequiredToBuy(new int[]{5, 1, 1, 1}, 0));
-        System.out.println(solution.timeRequiredToBuy(new int[]{84, 49, 5, 24, 70, 77, 87, 8}, 3));
-        System.out.println(solution.timeRequiredToBuy(new int[]{15, 66, 3, 47, 71, 27, 54, 43, 97, 34, 94, 33, 54, 26, 15, 52, 20, 71, 88, 42, 50, 6, 66, 88, 36, 99, 27, 82, 7, 72}, 18));
+        System.out.println("Expected = 6 Actual = " + solution.timeRequiredToBuy(new int[] {2, 3, 2}, 2));
+        System.out.println("Expected = 8 Actual = " + solution.timeRequiredToBuy(new int[] {5, 1, 1, 1}, 0));
+        System.out.println(
+            "Expected = 154 Actual = " + solution.timeRequiredToBuy(new int[] {84, 49, 5, 24, 70, 77, 87, 8}, 3));
+        System.out.println("Expected = 1457 Actual = " + solution.timeRequiredToBuy(
+            new int[] {15, 66, 3, 47, 71, 27, 54, 43, 97, 34, 94, 33, 54, 26, 15, 52, 20, 71, 88, 42, 50, 6, 66, 88, 36,
+                99, 27, 82, 7, 72}, 18));
     }
 }

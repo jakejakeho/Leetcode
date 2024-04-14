f0323ba1139e88f39847a94f07ec40307816127f
jakejakeho <mtho@connect.ust.hk>
2024-04-14 08:58:34 +0800

    Leetcode

---
+++ src/src/com/leetcode/s404/Solution.java
@@ -0,0 +1,33 @@
+package src.com.leetcode.s404;
+
+import src.com.utils.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        int sum = 0;
+        if (root == null) {
+            return sum;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+        sum += sumOfLeftLeaves(root.left);
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}

699a4a3e0cd0fb2c73c339b002d51017dd782627
jakejakeho <mtho@connect.ust.hk>
2024-04-19 08:18:47 +0800

    Leetcode

--- src/src/com/leetcode/s200/Solution.java
+++ src/src/com/leetcode/s200/Solution.java
@@ -1,50 +1,35 @@
 package src.com.leetcode.s200;
 
-public class Solution {
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.numIslands(new char[][]{
-                {'1', '1', '1', '1', '0'},
-                {'1', '1', '0', '1', '0'},
-                {'1', '1', '0', '0', '0'},
-                {'0', '0', '0', '0', '0'},
-        }));
-    }
-
+class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
+        int numOfIslands = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    visitAllOne(grid, i, j);
-                    count++;
+                if (visitIsland(grid, i, j)) {
+                    numOfIslands++;
                 }
             }
         }
-        return count;
+        return numOfIslands;
     }
 
-    private void visitAllOne(char[][] grid, int i, int j) {
-        if (grid[i][j] == '1') {
-            grid[i][j] = '0';
-
-            // go up
-            if (i - 1 >= 0)
-                visitAllOne(grid, i - 1, j);
-
-            // go right
-            if (j + 1 < grid[0].length)
-                visitAllOne(grid, i, j + 1);
-
-            // go down
-            if (i + 1 < grid.length)
-                visitAllOne(grid, i + 1, j);
-
-            // go left
-            if (j - 1 >= 0)
-                visitAllOne(grid, i, j - 1);
-
+    public boolean visitIsland(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+            return false;
         }
+
+        if (grid[i][j] != '1') {
+            return false;
+        }
+        grid[i][j] = '2';
+        // go up
+        visitIsland(grid, i - 1, j);
+        // go down
+        visitIsland(grid, i + 1, j);
+        // go left
+        visitIsland(grid, i, j - 1);
+        // go right
+        visitIsland(grid, i, j + 1);
+        return true;
     }
 }

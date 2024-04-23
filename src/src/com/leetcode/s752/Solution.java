bd692b5044ce417db444d4bcb45f9ab7b2c65dd0
jakejakeho <mtho@connect.ust.hk>
2024-04-23 08:12:28 +0800

    Leetcode

--- src/src/com/leetcode/s752/Solution.java
+++ src/src/com/leetcode/s752/Solution.java
@@ -1,35 +1,76 @@
 package src.com.leetcode.s752;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
+    Map<Character, Character> nextSlot = Map.of(
+            '0', '1',
+            '1', '2',
+            '2', '3',
+            '3', '4',
+            '4', '5',
+            '5', '6',
+            '6', '7',
+            '7', '8',
+            '8', '9',
+            '9', '0'
+    );
+    // Map the previous slot digit for each current slot digit.
+    Map<Character, Character> prevSlot = Map.of(
+            '0', '9',
+            '1', '0',
+            '2', '1',
+            '3', '2',
+            '4', '3',
+            '5', '4',
+            '6', '5',
+            '7', '6',
+            '8', '7',
+            '9', '8'
+    );
+
     public int openLock(String[] deadends, String target) {
-        Set<String> deadEndSet = new HashSet<>(Arrays.asList(deadends));
+        Set<String> visited = new HashSet<>(Arrays.asList(deadends));
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("0000");
 
-        StringBuilder stringBuilder = new StringBuilder(target);
-        int match = 0;
-        for (int i = 0; i < stringBuilder.length(); i++) {
-            stringBuilder.setCharAt(i, (char)(stringBuilder.charAt(i) + 1));
-            if (deadEndSet.contains(stringBuilder.toString())) {
-                match++;
-            }
-            stringBuilder.setCharAt(i, (char)(stringBuilder.charAt(i) - 2));
-            if (deadEndSet.contains(stringBuilder.toString())) {
-                match++;
-            }
-            stringBuilder.setCharAt(i, (char)(stringBuilder.charAt(i) + 1));
-        }
+        int turn = 0;
 
-        if (match == 8) {
+        if (visited.contains("0000")) {
             return -1;
         }
 
-        StringBuilder pickLock = new StringBuilder("0000");
-        int turn = 0;
-        while (!pickLock.equals(target)) {
-
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String current = queue.poll();
+                if (Objects.equals(current, target)) {
+                    return turn;
+                }
+                visited.add(current);
+                for (int j = 0; j < 4; j++) {
+                    StringBuilder sb = new StringBuilder(current);
+                    sb.setCharAt(j, nextSlot.get(current.charAt(j)));
+                    String plus = sb.toString();
+                    if (!visited.contains(plus)) {
+                        queue.offer(plus);
+                        visited.add(plus);
+                    }
+                    sb.setCharAt(j, prevSlot.get(current.charAt(j)));
+                    String minus = sb.toString();
+                    if (!visited.contains(minus)) {
+                        queue.offer(minus);
+                        visited.add(minus);
+                    }
+                }
+            }
+            turn++;
         }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.openLock(new String[]{"8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"}, "8888"));
     }
 }

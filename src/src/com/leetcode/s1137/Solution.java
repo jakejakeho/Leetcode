65b967450f3c8f35c196361bf846913ee345a5d7
jakejakeho <mtho@connect.ust.hk>
2024-04-24 08:16:31 +0800

    Leetcode

--- src/src/com/leetcode/s1137/Solution.java
+++ src/src/com/leetcode/s1137/Solution.java
@@ -5,6 +5,7 @@ class Solution {
         int T0 = 0;
         int T1 = 1;
         int T2 = 1;
+        int T3 = -1;
         if (n == 0) {
             return T0;
         } else if (n == 1) {
@@ -12,7 +13,6 @@ class Solution {
         } else if (n == 2) {
             return T2;
         }
-        int T3 = 0;
         for (int i = 3; i <= n; i++) {
             T3 = T0 + T1 + T2;
             T0 = T1;
@@ -20,10 +20,5 @@ class Solution {
             T2 = T3;
         }
         return T3;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.tribonacci(4));
     }
 }

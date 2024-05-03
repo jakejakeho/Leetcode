fd9dee342a4855e16a8d649961ef6245f2c25783
jake.ho <jake.ho@okg.com>
2024-05-03 09:56:14 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s165/Solution.java
+++ src/src/com/leetcode/s165/Solution.java
@@ -30,5 +30,6 @@ class Solution {
         System.out.println(solution.compareVersion("1.01", "1.001"));
         System.out.println(solution.compareVersion("1.0", "1.0.0"));
         System.out.println(solution.compareVersion("0.1", "1.1"));
+        System.out.println(solution.compareVersion("1.1", "1"));
     }
 }

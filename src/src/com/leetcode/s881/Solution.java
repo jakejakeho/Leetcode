a438b6be0853ac74a3871f7b36f68fd4d618fd78
jake.ho <jake.ho@okg.com>
2024-05-04 16:01:05 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s881/Solution.java
+++ src/src/com/leetcode/s881/Solution.java
@@ -4,10 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("expected = 1, result = " + solution.numRescueBoats(new int[] {1, 2}, 3));
+        System.out.println("expected = 3, result = " + solution.numRescueBoats(new int[] {3, 2, 2, 1}, 3));
+        System.out.println("expected = 4, result = " + solution.numRescueBoats(new int[] {3, 5, 3, 4}, 5));
+        System.out.println("expected = 3, result = " + solution.numRescueBoats(new int[] {3, 2, 3, 2, 2}, 6));
+    }
+
     public int numRescueBoats(int[] people, int limit) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int person : people) {
-            map.put(person, map.getOrDefault(person, 0) + 1);
+        Arrays.sort(people);
+        int num = 0;
+        int left = 0;
+        int right = people.length - 1;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            num++;
         }
+        return num;
     }
 }

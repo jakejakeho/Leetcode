fb2b16b3f8cf52a4d00ed80fff9820a61326bbcb
jakejakeho <mtho@connect.ust.hk>
2024-05-04 08:50:21 +0800

    Leetcode

--- src/src/com/leetcode/s881/Solution.java
+++ src/src/com/leetcode/s881/Solution.java
@@ -1,29 +1,13 @@
 package src.com.leetcode.s881;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println("expected = 1, result = " + solution.numRescueBoats(new int[]{1, 2}, 3));
-        System.out.println("expected = 3, result = " + solution.numRescueBoats(new int[]{3, 2, 2, 1}, 3));
-        System.out.println("expected = 4, result = " + solution.numRescueBoats(new int[]{3, 5, 3, 4}, 5));
-        System.out.println("expected = 3, result = " + solution.numRescueBoats(new int[]{3, 2, 3, 2, 2}, 6));
-    }
-
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
-        int boats = 0;
-        int left = 0;
-        int right = people.length - 1;
-        while (left <= right) {
-            boats++;
-            if (left <= right && people[left] <= limit - people[right]) {
-                left++;
-            }
-            right--;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int person : people) {
+            map.put(person, map.getOrDefault(person, 0) + 1);
         }
-        return boats;
     }
 }

1745468e564898878c241bfd0c5653e0b19cb5cb
jakejakeho <mtho@connect.ust.hk>
2024-05-05 08:46:04 +0800

    Leetcode

---
+++ src/src/com/leetcode/s237/Solution.java
@@ -0,0 +1,18 @@
+package src.com.leetcode.s237;
+
+import src.com.leetcode.s206.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}

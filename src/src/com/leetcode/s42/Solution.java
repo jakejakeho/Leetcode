06af9d61a8cf890919cb087481bdc140c7065354
jakejakeho <mtho@connect.ust.hk>
2024-05-07 21:18:16 +0800

    Leetcode

--- src/src/com/leetcode/s42/Solution.java
+++ src/src/com/leetcode/s42/Solution.java
@@ -1,44 +1,12 @@
 package src.com.leetcode.s42;
 
-import java.util.Arrays;
-
-import static java.lang.Math.min;
-
 class Solution {
-
     public int trap(int[] height) {
-        int total = 0;
-
-        int leftPointer = 0;
-        int rightPointer = height.length - 1;
-
-        int leftMax = height[leftPointer];
-        int rightMax = height[rightPointer];
-
-        while (leftPointer < rightPointer) {
-            if (leftMax < rightMax) {
-                int water = leftMax - height[leftPointer];
-                if (water >= 0) {
-                    total += water;
-                }
-                leftPointer++;
-                leftMax = Math.max(leftMax, height[leftPointer]);
-            } else {
-                int water = rightMax - height[rightPointer];
-                if (water >= 0) {
-                    total += water;
-                }
-                rightPointer--;
-                rightMax = Math.max(rightMax, height[rightPointer]);
-            }
-        }
-        return total;
+        return -1;
     }
 
     public static void main(String[] args) {
-        int[] height = new int[] {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         Solution solution = new Solution();
-        System.out.println("Input: " + Arrays.toString(height));
-        System.out.println("Output: " + solution.trap(height));
+        System.out.println(solution.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
 }

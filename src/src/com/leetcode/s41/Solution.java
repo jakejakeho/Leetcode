706aa6dda1ea282dec20f53cf222a74bb7ca9d8f
jakejakeho <mtho@connect.ust.hk>
2024-05-09 08:19:28 +0800

    Leetcode

--- src/src/com/leetcode/s41/Solution.java
+++ src/src/com/leetcode/s41/Solution.java
@@ -2,42 +2,30 @@ package src.com.leetcode.s41;
 
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        int temp = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < 0 || nums[i] > nums.length + 1) {
-                nums[i] = 0;
-            }
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == -1)
-                continue;
-            int index = nums[i] - 1;
-            if (index < 0 || index >= nums.length)
-                continue;
-            if (nums[index] > 0) {
-                nums[i] = nums[index];
+            if (nums[i] >= 1 && nums[i] <= nums.length && nums[i] != (i + 1) && nums[nums[i] - 1] != nums[i]) {
+                temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
                 i--;
             }
-            nums[index] = -1;
         }
 
-        int smallest = 1;
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                smallest++;
-            } else {
-                break;
+            if (nums[i] != (i + 1)) {
+                return i + 1;
             }
         }
-        return smallest;
+        return nums.length + 1;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        System.out.println(solution.firstMissingPositive(new int[]{1, 2, 0}));
-
-//        System.out.println(solution.firstMissingPositive(new int[]{3, 4, -1, 1}));
-
+        System.out.println(solution.firstMissingPositive(new int[]{1, 2, 0}));
+        System.out.println(solution.firstMissingPositive(new int[]{3, 4, -1, 1}));
         System.out.println(solution.firstMissingPositive(new int[]{7, 8, 9, 11, 12}));
+        System.out.println(solution.firstMissingPositive(new int[]{1, 1}));
     }
 }

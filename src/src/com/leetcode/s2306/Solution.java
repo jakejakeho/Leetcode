58796f7849e33d3ffaa7fc51c4a2625554e71f96
jakejakeho <mtho@connect.ust.hk>
2024-05-11 20:31:52 +0800

    Leetcode

--- src/src/com/leetcode/s2306/Solution.java
+++ src/src/com/leetcode/s2306/Solution.java
@@ -4,27 +4,36 @@ import java.util.*;
 
 class Solution {
     public long distinctNames(String[] ideas) {
-        Map<String, Set<Character>> map = new HashMap<>();
+        long distinct = 0;
+        Map<Character, Set<String>> map = new HashMap<>();
         for (String idea : ideas) {
-            String suffix = idea.substring(1);
-            Character prefix = idea.charAt(0);
-            map.computeIfAbsent(suffix, (k) -> new HashSet<>());
-            map.get(suffix).add(prefix);
+            map.computeIfAbsent(idea.charAt(0), (k) -> new HashSet<>());
+            map.get(idea.charAt(0)).add(idea.substring(1));
         }
 
-        List<Map.Entry<String, Set<Character>>> list = new ArrayList<>(map.size());
-        list.addAll(map.entrySet());
+        for (int i = 0; i < 26; i++) {
+            for (int j = i + 1; j < 26; j++) {
+                Character k1 = (char) ('a' + i);
+                Character k2 = (char) ('a' + j);
+                if (i == j || !map.containsKey(k1) || !map.containsKey(k2))
+                    continue;
+                Set<String> s1 = map.get(k1);
+                Set<String> s2 = map.get(k2);
+                int s1Count = 0;
+                for (String s : s1) {
+                    if (!s2.contains(s)) {
+                        s1Count++;
+                    }
+                }
 
-        long distinct = 0;
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                Map.Entry<String, Set<Character>> e1 = list.get(i);
-                Map.Entry<String, Set<Character>> e2 = list.get(j);
-                Set<Character> s1 = e1.getValue();
-                Set<Character> s2 = e2.getValue();
-                Set<Character> intersection = new HashSet<>(s1);
-                intersection.retainAll(s2);
-                distinct += 2 * (s1.size() - intersection.size()) * (s2.size() - intersection.size());
+                int s2Count = 0;
+                for (String s : s2) {
+                    if (!s1.contains(s)) {
+                        s2Count++;
+                    }
+                }
+                long com = 2 * ((long) s1Count * s2Count);
+                distinct += com;
             }
         }
         return distinct;

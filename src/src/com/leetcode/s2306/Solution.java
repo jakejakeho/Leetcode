6e403773fa51e9e82b7ca7e62b3aca1bc26d1c78
jakejakeho <mtho@connect.ust.hk>
2024-05-11 20:39:32 +0800

    Leetcode

--- src/src/com/leetcode/s2306/Solution.java
+++ src/src/com/leetcode/s2306/Solution.java
@@ -5,20 +5,20 @@ import java.util.*;
 class Solution {
     public long distinctNames(String[] ideas) {
         long distinct = 0;
-        Map<Character, Set<String>> map = new HashMap<>();
+        HashSet<String>[] map = new HashSet[26];
+        for (int i = 0 ;i < 26;i ++) {
+            map[i] = new HashSet<>();
+        }
         for (String idea : ideas) {
-            map.computeIfAbsent(idea.charAt(0), (k) -> new HashSet<>());
-            map.get(idea.charAt(0)).add(idea.substring(1));
+            map[idea.charAt(0) - 'a'].add(idea.substring(1));
         }
 
         for (int i = 0; i < 26; i++) {
             for (int j = i + 1; j < 26; j++) {
-                Character k1 = (char) ('a' + i);
-                Character k2 = (char) ('a' + j);
-                if (i == j || !map.containsKey(k1) || !map.containsKey(k2))
+                if (i == j)
                     continue;
-                Set<String> s1 = map.get(k1);
-                Set<String> s2 = map.get(k2);
+                Set<String> s1 = map[i];
+                Set<String> s2 = map[j];
                 int s1Count = 0;
                 for (String s : s1) {
                     if (!s2.contains(s)) {

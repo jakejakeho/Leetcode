60088c31fe3bb0f1962808b2c45bb319525ff69a
jakejakeho <mtho@connect.ust.hk>
2024-05-15 20:39:10 +0800

    Leetcode

--- src/src/com/leetcode/s1658/Solution.java
+++ src/src/com/leetcode/s1658/Solution.java
@@ -1,24 +1,51 @@
 package src.com.leetcode.s1658;
-public class Solution {
+
+class Solution {
     public int minOperations(int[] nums, int x) {
-        int target = -x, n = nums.length;
-        for (int num : nums) target += num;
-
-        if (target == 0) return n;
-
-        int maxLen = 0, curSum = 0, left = 0;
-
-        for (int right = 0; right < n; ++right) {
-            curSum += nums[right];
-            while (left <= right && curSum > target) {
-                curSum -= nums[left];
-                left++;
-            }
-            if (curSum == target) {
-                maxLen = Math.max(maxLen, right - left + 1);
-            }
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
         }
 
-        return maxLen != 0 ? n - maxLen : -1;
+        if (sum < x)
+            return -1;
+        int target = sum - x;
+
+        int currentSum = 0;
+        int maxLeft = -1;
+        int maxRight = -1;
+        int left = 0;
+        int right = 0;
+        while (left <= right && right < nums.length) {
+            currentSum += nums[right];
+            while (currentSum > target) {
+                currentSum -= nums[left];
+                left++;
+            }
+
+            if (currentSum == target) {
+                int length = (right - left + 1);
+                int currentLength = (maxRight - maxLeft + 1);
+                if (maxLeft == -1 || length > currentLength) {
+                    maxLeft = left;
+                    maxRight = right;
+                }
+            }
+            right++;
+        }
+
+        if (maxLeft == -1) {
+            return -1;
+        }
+        return nums.length - (maxRight - maxLeft + 1);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minOperations(new int[]{1, 1, 4, 2, 3}, 5));
+        System.out.println(solution.minOperations(new int[]{5, 6, 7, 8, 9}, 4));
+        System.out.println(solution.minOperations(new int[]{3, 2, 20, 1, 1, 3,}, 10));
+        System.out.println(solution.minOperations(new int[]{5, 2, 3, 1, 1,}, 5));
+        System.out.println(solution.minOperations(new int[]{1, 1,}, 3));
     }
 }

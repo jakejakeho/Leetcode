653018eda935c36850048578fb4428d8fb9fbce8
jake.ho <jake.ho@okg.com>
2024-05-22 08:36:07 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s131/Solution.java
+++ src/src/com/leetcode/s131/Solution.java
@@ -4,37 +4,43 @@ import java.util.List;
 
 class Solution {
 
-    public static void main(String[] args) {
-        String s = "aab";
-        Solution solution = new Solution();
-        //System.out.println(solution.partition(s));
-        s = "bb";
-        System.out.println(solution.partition(s));
-    }
-
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        if (s.equals("")) {
-            res.add(new ArrayList<String>());
-            return res;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            if (isPalindrome(s, i + 1)) {
-                for (List<String> list : partition(s.substring(i + 1))) {
-                    list.add(0, s.substring(0, i + 1));
-                    res.add(list);
-                }
-            }
-        }
-        return res;
+        List<List<String>> result = new ArrayList<>();
+        List<String> currentList = new ArrayList<>();
+        int index = 0;
+        backtracking(s, result, currentList, index);
+        return result;
     }
 
-    private boolean isPalindrome(String s, int n) {
-        for (int i = 0; i < n / 2.0; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                return false;
+    private void backtracking(String s, List<List<String>> result, List<String> currentList, int index) {
+        if (index == s.length()) {
+            result.add(new ArrayList<>(currentList));
+            return;
+        }
+
+        for (int i = index; i < s.length(); i++) {
+            if (isPalindrome(s, index, i)) {
+                String substr = s.substring(index, i + 1);
+                currentList.add(substr);
+                backtracking(s, result, currentList, i + 1);
+                currentList.remove(currentList.size() - 1);
             }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end))
+                return false;
+            start++;
+            end--;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.partition("aab"));
+        System.out.println(solution.partition("a"));
     }
 }

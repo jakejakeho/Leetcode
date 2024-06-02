8620e22cd671db440489cbb27e0373b999b5aad8
jakejakeho <mtho@connect.ust.hk>
2024-06-02 09:36:58 +0800

    Leetcode

--- src/src/com/leetcode/s119/Solution.java
+++ src/src/com/leetcode/s119/Solution.java
@@ -1,30 +1,23 @@
 package src.com.leetcode.s119;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        if (rowIndex == 0) {
-            return List.of(1);
-        } else if (rowIndex == 1) {
-            return List.of(1, 1);
-        }
-        int[] lastRow = new int[rowIndex + 1];
-        lastRow[0] = 1;
-        lastRow[1] = 1;
-        for (int i = 2; i <= rowIndex; i++) {
-            int[] currentRow = new int[i + 1];
-            currentRow[0] = 1;
-            for (int j = 1; j <= i - 1; j++) {
-                currentRow[j] = lastRow[j - 1] + lastRow[j];
+        int[] row = new int[rowIndex + 1];
+        Arrays.fill(row, 1);
+
+        for (int i = 2; i < rowIndex + 1; i++) {
+            for (int j = 1; j < i; j++) {
+                row[i - j] += row[i - j - 1];
             }
-            currentRow[i] = 1;
-            lastRow = currentRow;
         }
-        List<Integer> list = new ArrayList<>(lastRow.length);
-        for (int i = 0; i < lastRow.length; i++) {
-            list.add(lastRow[i]);
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < row.length; i++) {
+            list.add(row[i]);
         }
         return list;
     }

0dcdadb610cdf05afcd3c2cfb974c98e2a59296f
jakejakeho <mtho@connect.ust.hk>
2024-06-03 20:58:47 +0800

    Leetcode

--- src/src/com/leetcode/s349/Solution.java
+++ src/src/com/leetcode/s349/Solution.java
@@ -1,25 +1,34 @@
 package src.com.leetcode.s349;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> nums1Map = new HashSet<>();
-        for (int num1: nums1) {
-            nums1Map.add(num1);
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
         }
 
         Set<Integer> intersection = new HashSet<>();
-        for (int num2: nums2) {
-            if (nums1Map.contains(num2))
-            intersection.add(num2);
+        for (int j = 0; j < nums2.length; j++) {
+            if (set.contains(nums2[j])) {
+                intersection.add(nums2[j]);
+            }
         }
 
-        int[] intersectionArr = new int[intersection.size()];
-        int count = 0;
-        for(int ele : intersection) {
-            intersectionArr[count++] = ele;
+        int[] ans = new int[intersection.size()];
+        int index = 0;
+        for (int num : intersection) {
+            ans[index++] = num;
         }
-        return intersectionArr;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+        System.out.println(Arrays.toString(solution.intersection(new int[]{9, 4}, new int[]{9, 4, 9, 8, 4})));
     }
 }

b036309b4fc86b694db4f859c6ef99907b34dcc2
jakejakeho <mtho@connect.ust.hk>
2024-06-08 17:14:20 +0800

    Leetcode

--- src/src/com/leetcode/s1685/Solution.java
+++ src/src/com/leetcode/s1685/Solution.java
@@ -4,33 +4,14 @@ import java.util.Arrays;
 
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
-        int[] prefixSum = new int[nums.length];
-        int[] postfixSum = new int[nums.length];
-        int leftSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            leftSum += nums[i];
-            prefixSum[i] = leftSum;
-        }
-
-        int rightSum = 0;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            rightSum += nums[i];
-            postfixSum[i] = rightSum;
-        }
-
         int[] result = new int[nums.length];
+        int[] prefixSum = new int[nums.length + 1];
         for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            if (i != 0) {
-                sum += i * nums[i];
-                System.out.println(sum);
-                sum -= prefixSum[i - 1];
-            }
-            if (i != nums.length - 1) {
-                sum += postfixSum[i + 1];
-                sum -= (nums.length - 1 - i) * nums[i];
-            }
-            result[i] = sum;
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = (i * nums[i]) - prefixSum[i] + (prefixSum[nums.length] - prefixSum[i + 1] - (nums.length - i - 1) * nums[i]);
         }
         return result;
     }

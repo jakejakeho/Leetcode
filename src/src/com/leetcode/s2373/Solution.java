f5a936237a3430aa39c862ff58b51e0a9998f66f
jakejakeho <mtho@connect.ust.hk>
2024-07-06 10:35:10 +0800

    Leetcode

--- src/src/com/leetcode/s2373/Solution.java
+++ src/src/com/leetcode/s2373/Solution.java
@@ -4,19 +4,24 @@ import java.util.Arrays;
 
 class Solution {
     public int[][] largestLocal(int[][] grid) {
-        int[][] result = new int[grid.length - 2][grid.length - 2];
-        for (int i = 1; i < grid.length - 1; i++) {
-            for (int j = 1; j < grid[0].length - 1; j++) {
-                int max = 0;
+        int[][] largestLocal = new int[grid.length - 2][grid[0].length - 2];
+        int tempI = 0;
+        int tempJ = 0;
+        for (int i = 1; i <= grid.length - 2; i++) {
+            for (int j = 1; j <= grid[0].length - 2; j++) {
+                int largest = 0;
                 for (int k = i - 1; k <= i + 1; k++) {
                     for (int l = j - 1; l <= j + 1; l++) {
-                        max = Math.max(max, grid[k][l]);
+                        largest = Math.max(largest, grid[k][l]);
                     }
                 }
-                result[i - 1][j - 1] = max;
+                largestLocal[tempI][tempJ] = largest;
+                tempJ++;
             }
+            tempI++;
+            tempJ = 0;
         }
-        return result;
+        return largestLocal;
     }
 
     public static void main(String[] args) {
@@ -25,7 +30,14 @@ class Solution {
                 {9, 9, 8, 1},
                 {5, 6, 2, 6},
                 {8, 2, 6, 4},
-                {6, 2, 2, 2}
+                {6, 2, 2, 2},
+        })));
+        System.out.println(Arrays.deepToString(solution.largestLocal(new int[][]{
+                {1,1,1,1,1},
+                {1,1,1,1,1},
+                {1,1,2,1,1},
+                {1,1,1,1,1},
+                {1,1,1,1,1},
         })));
     }
 }

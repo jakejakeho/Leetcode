e0291c71b7d615f2f0c6b0d652ca25d6e93ff53c
jakejakeho <mtho@connect.ust.hk>
2024-07-21 09:51:43 +0800

    Leetcode

--- src/src/com/leetcode/s2864/Solution.java
+++ src/src/com/leetcode/s2864/Solution.java
@@ -1,26 +1,38 @@
 package src.com.leetcode.s2864;
 
+import java.util.Map;
+
 class Solution {
     public String maximumOddBinaryNumber(String s) {
-        StringBuilder result = new StringBuilder(s.length());
-        int numOfOne = 0;
-        int numOfZero = 0;
+        int zeroCount = 0;
+        int oneCount = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '0') {
-                numOfZero++;
+                zeroCount++;
             } else {
-                numOfOne++;
+                oneCount++;
             }
         }
-        result.append("1".repeat(Math.max(0, numOfOne - 1)));
-        result.append("0".repeat(Math.max(0, numOfZero)));
-        result.append('1');
+        StringBuilder result = new StringBuilder();
+        while (oneCount > 1) {
+            result.append('1');
+            oneCount--;
+        }
+        while (zeroCount > 0) {
+            result.append('0');
+            zeroCount--;
+
+        }
+        while (oneCount > 0) {
+            result.append('1');
+            oneCount--;
+        }
         return result.toString();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.maximumOddBinaryNumber("010"));
-        System.out.println(solution.maximumOddBinaryNumber("1001"));
+        System.out.println(solution.maximumOddBinaryNumber("0101"));
     }
 }

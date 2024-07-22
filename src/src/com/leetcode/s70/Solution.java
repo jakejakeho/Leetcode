bf3807f7fcbb39abcf28374387dcab078ac10a1b
jakejakeho <mtho@connect.ust.hk>
2024-07-22 22:07:21 +0800

    Leetcode

--- src/src/com/leetcode/s70/Solution.java
+++ src/src/com/leetcode/s70/Solution.java
@@ -1,19 +1,27 @@
 package src.com.leetcode.s70;
 
+import java.util.Arrays;
+
 class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] cache = new int[n + 1];
+        Arrays.fill(cache, -1);
+        cache[0] = 0;
+        cache[1] = 1;
+        cache[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+        return cache[n];
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        System.out.println(solution.climbStairs(2));
         System.out.println(solution.climbStairs(3));
-    }
-
-    public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        return dp[n];
+        System.out.println(solution.climbStairs(10));
     }
 }

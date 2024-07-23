572c305360eef696f047775405b8997f5692ebd0
jakejakeho <mtho@connect.ust.hk>
2024-07-23 09:46:31 +0800

    Leetcode

--- src/src/com/leetcode/s198/Solution.java
+++ src/src/com/leetcode/s198/Solution.java
@@ -1,13 +1,20 @@
 package src.com.leetcode.s198;
+
 class Solution {
     public int rob(int[] nums) {
-        int rob1 = 0;
-        int rob2 = 0;
-        for (int num : nums) {
-            int temp = Math.max(rob1 + num, rob2);
-            rob1 = rob2;
-            rob2 = temp;
+        int[] cache = new int[nums.length];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int current = nums[i];
+            int next = (i + 1) < nums.length ? cache[i + 1] : 0;
+            int nextNext = (i + 2) < nums.length ? cache[i + 2] : 0;
+            cache[i] = Math.max(next, current + nextNext);
         }
-        return rob2;
+        return cache[0];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.rob(new int[]{1, 2, 3, 1}));
+        System.out.println(solution.rob(new int[]{2, 7, 9, 3, 1}));
     }
 }

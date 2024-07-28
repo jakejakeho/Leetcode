7eb7e7f19105b4eb43d08cffea9d429485acef51
jakejakeho <mtho@connect.ust.hk>
2024-07-28 23:08:45 +0800

    Leetcode

---
+++ src/src/com/leetcode/s383/Solution.java
@@ -0,0 +1,28 @@
+package src.com.leetcode.s383;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            map.put(ransomNote.charAt(i), map.getOrDefault(ransomNote.charAt(i), 0) - 1);
+            if (map.getOrDefault(ransomNote.charAt(i), 0) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.canConstruct("a", "b"));
+        System.out.println(solution.canConstruct("aa", "ab"));
+        System.out.println(solution.canConstruct("aa", "aab"));
+    }
+}

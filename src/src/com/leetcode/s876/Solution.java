f949333173765ad6702d742db12ccd913f246f58
jakejakeho <mtho@connect.ust.hk>
2024-07-28 23:05:37 +0800

    Leetcode

--- src/src/com/leetcode/s876/Solution.java
+++ src/src/com/leetcode/s876/Solution.java
@@ -5,25 +5,30 @@ import src.com.leetcode.s206.ListNode;
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode slowPointer = head;
-        ListNode fastPointer = head.next;
-        while (fastPointer != null) {
-            slowPointer = slowPointer.next;
-            if (fastPointer.next != null) {
-                fastPointer = fastPointer.next.next;
-            } else {
-                fastPointer = null;
-            }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return slowPointer;
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
+        System.out.println(solution.middleNode(l1));
     }
 }

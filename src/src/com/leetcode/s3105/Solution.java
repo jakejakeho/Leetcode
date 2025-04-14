76b63975379a7e10a5101e6dc2f3cdfee3edc1fe
jake.ho <jake.ho@okg.com>
2025-04-14 16:25:40 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s3105/Solution.java
+++ src/src/com/leetcode/s3105/Solution.java
@@ -1,25 +1,43 @@
 package src.com.leetcode.s3105;
 
 class Solution {
+
     public int longestMonotonicSubarray(int[] nums) {
-        int longest = 1;
-        int increasing = 1;
-        int decreasing = 1;
-        for (int i = 0; i < nums.length - 1; i++) {
-            int diff = nums[i + 1] - nums[i];
-            if (diff > 0) {
-                increasing++;
-                longest = Math.max(longest, increasing);
-                decreasing = 1;
-            } else if (diff < 0) {
-                increasing = 1;
-                decreasing++;
-                longest = Math.max(longest, decreasing);
-            } else {
-                increasing = 1;
-                decreasing = 1;
+        int longest = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int last = nums[i];
+            int length = 1;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > last) {
+                    last = nums[j];
+                    length++;
+                } else {
+                    break;
+                }
             }
+            longest = Math.max(longest, length);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int last = nums[i];
+            int length = 1;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < last) {
+                    last = nums[j];
+                    length++;
+                } else {
+                    break;
+                }
+            }
+            longest = Math.max(longest, length);
         }
         return longest;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestMonotonicSubarray(new int[] {1, 4, 3, 3, 2}));
+        System.out.println(solution.longestMonotonicSubarray(new int[] {3, 3, 3, 3}));
+        System.out.println(solution.longestMonotonicSubarray(new int[] {3, 2, 1}));
     }
 }

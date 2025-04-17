ba2b840f6cdef6ef3ca7ab5ea55596d8e795a7a7
jake.ho <jake.ho@okg.com>
2025-04-17 17:18:45 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s1002/Solution.java
+++ src/src/com/leetcode/s1002/Solution.java
@@ -1,32 +1,37 @@
 package src.com.leetcode.s1002;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Solution {
+
     public List<String> commonChars(String[] words) {
         List<String> result = new ArrayList<>();
-        int[] exist = null;
+        int[] count = new int[26];
+        Arrays.fill(count, Integer.MAX_VALUE);
         for (String word : words) {
-            int[] current = new int[26];
-            for (char c : word.toCharArray()) {
-                current[c - 'a']++;
+            int[] newCount = new int[26];
+            for (int i = 0; i < word.length(); i++) {
+                newCount[word.charAt(i) - 'a']++;
             }
-            if (exist == null) {
-                exist = current;
-            } else {
-                for (int j = 0; j < 26; j++) {
-                    exist[j] = Math.min(exist[j], current[j]);
-                }
+            for (int i = 0; i < count.length; i++) {
+                count[i] = Math.min(count[i], newCount[i]);
             }
         }
 
-        for (int i = 0; i < 26; i++) {
-            while (exist[i] > 0) {
-                result.add(String.valueOf((char) ('a' + i)));
-                exist[i]--;
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                result.add(String.valueOf((char)('a' + i)));
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.commonChars(new String[] {"bella", "label", "roller"}));
+        System.out.println(solution.commonChars(new String[] {"cool", "lock", "cook"}));
+        // []
+        System.out.println(solution.commonChars(new String[] {"acabcddd","bcbdbcbd","baddbadb","cbdddcac","aacbcccd","ccccddda","cababaab","addcaccd"}));
     }
 }

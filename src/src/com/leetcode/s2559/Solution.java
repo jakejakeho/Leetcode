e77dc27965e9b60c8b94b952be3a81117f3a2af2
jake.ho <jake.ho@okg.com>
2025-04-17 17:59:32 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

---
+++ src/src/com/leetcode/s2559/Solution.java
@@ -0,0 +1,45 @@
+package src.com.leetcode.s2559;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    private static final Set<Character> VOWEL_SET = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        boolean[] isVowel = new boolean[words.length];
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (VOWEL_SET.contains(word.charAt(0)) && VOWEL_SET.contains(word.charAt(word.length() - 1))) {
+                isVowel[i] = true;
+            }
+        }
+
+        int[] isVowelTotal = new int[words.length + 1];
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (isVowel[i])
+                count++;
+            isVowelTotal[i + 1] = count;
+        }
+
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int lower = queries[i][0];
+            int upper = queries[i][1];
+            if (lower >= 0 && lower < words.length && upper >= 0 && upper < words.length) {
+                result[i] = isVowelTotal[upper + 1] - isVowelTotal[lower];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.vowelStrings(new String[] {"aba", "bcb", "ece", "aa", "e"},
+            new int[][] {{0, 2}, {1, 4}, {1, 1}})));
+        System.out.println(
+            Arrays.toString(solution.vowelStrings(new String[] {"a", "e", "i"}, new int[][] {{0, 2}, {0, 1}, {2, 2}})));
+    }
+}

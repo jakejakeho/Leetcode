acf709ef60972ff05c694ed6e2774c5df81c8e93
jake.ho <jake.ho@okg.com>
2025-04-17 18:14:40 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s1701/Solution.java
+++ src/src/com/leetcode/s1701/Solution.java
@@ -1,31 +1,26 @@
 package src.com.leetcode.s1701;
-
 class Solution {
+
     public double averageWaitingTime(int[][] customers) {
-        long totalWaitingTime = 0;
-        long currentTime = customers[0][0];
+        double totalWaitingTime = 0.0;
+        int chefTime = 0;
         for (int i = 0; i < customers.length; i++) {
-            if (customers[i][0] > currentTime) {
-                currentTime = customers[i][0];
+            int startTime;
+            if (customers[i][0] >= chefTime) {
+                startTime = customers[i][0];
+            } else {
+                startTime = chefTime;
             }
-            currentTime += customers[i][1];
-            totalWaitingTime += (currentTime - customers[i][0]);
+            chefTime = startTime + customers[i][1];
+            int waitingTime = chefTime - customers[i][0];
+            totalWaitingTime += waitingTime;
         }
-        return totalWaitingTime / (customers.length + 0.0);
+        return totalWaitingTime / customers.length;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.averageWaitingTime(new int[][]{
-                {1, 2},
-                {2, 5},
-                {4, 3}
-        }));
-        System.out.println(solution.averageWaitingTime(new int[][]{
-                {5, 2},
-                {5, 4},
-                {10, 3},
-                {20, 1},
-        }));
+        System.out.println(solution.averageWaitingTime(new int[][] {{1, 2}, {2, 5}, {4, 3}}));
+        System.out.println(solution.averageWaitingTime(new int[][] {{5, 2}, {5, 4}, {10, 3}, {20, 1}}));
     }
 }

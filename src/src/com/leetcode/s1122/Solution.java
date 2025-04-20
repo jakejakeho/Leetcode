8011cd1f9ebbe4654f7ae3f8d7d8c08bc85abef3
jakejakeho <mtho@connect.ust.hk>
2025-04-20 19:54:18 +0800

    Leetcode

--- src/src/com/leetcode/s1122/Solution.java
+++ src/src/com/leetcode/s1122/Solution.java
@@ -1,19 +1,42 @@
 package src.com.leetcode.s1122;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer, Integer> priorityMap = new HashMap<>();
-        int arr2Length = arr2.length;
-        for (int i = 0; i < arr2Length; i++) {
-            priorityMap.put(arr2[i], i - arr2Length);
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < arr1.length; i++) {
+            map.put(arr1[i], map.getOrDefault(arr1[i], 0) + 1);
         }
-        return Arrays.stream(arr1).boxed().sorted(Comparator.comparingInt(a -> priorityMap.getOrDefault(a, a))).mapToInt(Integer::intValue).toArray();
+
+        int[] result = new int[arr1.length];
+        int index = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            int v2 = arr2[i];
+            if (map.containsKey(v2)) {
+                for (int j = 0; j < map.get(v2); j++) {
+                    result[index++] = v2;
+                }
+                map.remove(v2, map.get(v2));
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            for (int i = 0; i < value; i++) {
+                result[index++] = key;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.relativeSortArray(new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, new int[]{2, 1, 4, 3, 9, 6})));
+        System.out.println(Arrays.toString(solution.relativeSortArray(new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19},
+                new int[]{2, 1, 4, 3, 9, 6})));
+        System.out.println(Arrays.toString(solution.relativeSortArray(new int[]{28, 6, 22, 8, 44, 17}, new int[]{22, 28, 8, 6})));
     }
 }

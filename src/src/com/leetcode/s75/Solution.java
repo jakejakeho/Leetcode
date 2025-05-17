b03c99762239fa7fe46dcf6ebfd8f819974f0528
jake.ho <jake.ho@okg.com>
2025-05-17 11:24:19 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s75/Solution.java
+++ src/src/com/leetcode/s75/Solution.java
@@ -1,27 +1,16 @@
 package src.com.leetcode.s75;
-
-import java.util.Arrays;
-
 class Solution {
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = new int[]{2, 0, 2, 1, 1, 0};
-        solution.sortColors(nums);
-        System.out.println(Arrays.toString(nums));
-    }
-
     public void sortColors(int[] nums) {
-        int[] counts = new int[3];
+        int[] count = new int[3];
         for (int num : nums) {
-            counts[num]++;
+            count[num]++;
         }
-
-        int pointer = 0;
-        for (int i = 0; i < counts.length; i++) {
-            int count = counts[i];
-            for (int j = 0; j < count; j++) {
-                nums[pointer++] = i;
+        int n = 0;
+        for (int i = 0; i < count.length; i++) {
+            int c = count[i];
+            for (int j = 0; j < c; j++) {
+                nums[n++] = i;
             }
         }
     }

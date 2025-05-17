e59952bfc0c60af2f1d39b7da4e8bf61063518cc
jake.ho <jake.ho@okg.com>
2025-05-17 08:34:45 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s2901/Solution.java
+++ src/src/com/leetcode/s2901/Solution.java
@@ -1,27 +1,50 @@
 package src.com.leetcode.s2901;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Solution {
+
     public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
-        List<String> result = new ArrayList<>();
-        result.add(words[0]);
-        int lastGroup = groups[0];
-        String lastStr = words[0];
-        for (int i = 1; i < words.length; i++) {
-            if (groups[i] != lastGroup
-                    && lastStr.length() == words[i].length()
-                    && hammingDistance(lastStr, words[i]) == 1) {
-                result.add(words[i]);
-                lastGroup = groups[i];
-                lastStr = words[i];
-            }
+        if (words.length == 1) {
+            return List.of(words[0]);
         }
+        Map<String, List<String>> cache = new HashMap<>();
+        return recursion(-1, 0, words, groups, cache);
+    }
+
+    private List<String> recursion(int prev, int current, String[] words, int[] groups,
+                                   Map<String, List<String>> cache) {
+        String key = prev + "-" + current;
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+        if (current >= words.length) {
+            return new ArrayList<>();
+        }
+        // not include this
+        List<String> notInclude = recursion(prev, current + 1, words, groups, cache);
+
+        // include this
+        List<String> includeThis = new ArrayList<>();
+        if (prev == -1 || (groups[current] != groups[prev] && hammingDistance(words[prev], words[current]) == 1)) {
+            includeThis = recursion(current, current + 1, words, groups, cache);
+            includeThis = new ArrayList<>(includeThis);
+            includeThis.addFirst(words[current]);
+        }
+
+        List<String> result = (includeThis.size() > notInclude.size()) ? includeThis : notInclude;
+
+        cache.put(key, result);
         return result;
     }
 
     private int hammingDistance(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return 0;
+        }
         int hammingDistance = 0;
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
@@ -33,7 +56,9 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.getWordsInLongestSubsequence(new String[]{"bab", "dab", "cab"}, new int[]{1, 2, 2}));
-        System.out.println(solution.getWordsInLongestSubsequence(new String[]{"a", "b", "c", "d"}, new int[]{1, 2, 3, 4}));
+        System.out.println(
+            solution.getWordsInLongestSubsequence(new String[] {"bab", "dab", "cab"}, new int[] {1, 2, 2}));
+        System.out.println(
+            solution.getWordsInLongestSubsequence(new String[] {"a", "b", "c", "d"}, new int[] {1, 2, 3, 4}));
     }
 }

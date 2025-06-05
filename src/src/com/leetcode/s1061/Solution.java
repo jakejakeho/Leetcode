df4c0b9bccd0980685fd73131359f8ded1c45435
jakejakeho <mtho@connect.ust.hk>
2025-06-05 09:19:07 +0800

    Leetcode

---
+++ src/src/com/leetcode/s1061/Solution.java
@@ -0,0 +1,68 @@
+package src.com.leetcode.s1061;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        Map<Character, Set<Character>> map = new HashMap<>();
+        char[] s1Arr = s1.toCharArray();
+        char[] s2Arr = s2.toCharArray();
+        for (int i = 0; i < s1Arr.length; i++) {
+            boolean s1Exist = map.containsKey(s1Arr[i]);
+            boolean s2Exist = map.containsKey(s2Arr[i]);
+            Set<Character> set;
+            if (!s1Exist && !s2Exist) {
+                set = new HashSet<>();
+            } else if (s1Exist && s2Exist) {
+                set = map.get(s1Arr[i]);
+                set.addAll(map.get(s2Arr[i]));
+                Set<Character> set2 = map.get(s2Arr[i]);
+                for (Character c : set2) {
+                    map.put(c, set);
+                }
+            } else if (s1Exist) {
+                set = map.get(s1Arr[i]);
+            } else {
+                set = map.get(s2Arr[i]);
+            }
+            set.add(s1Arr[i]);
+            set.add(s2Arr[i]);
+            map.put(s1Arr[i], set);
+            map.put(s2Arr[i], set);
+        }
+
+        Map<Character, Character> sortedMap = new HashMap<>();
+        for (Map.Entry<Character, Set<Character>> entry : map.entrySet()) {
+            Character c = entry.getKey();
+            Set<Character> set = entry.getValue();
+            Character smallest = 'z';
+            for (Character s : set) {
+                if (s < smallest) {
+                    smallest = s;
+                }
+            }
+            sortedMap.put(c, smallest);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder(baseStr.length());
+        char[] baseStrArr = baseStr.toCharArray();
+        for (int i = 0;  i< baseStrArr.length; i++) {
+            if (sortedMap.containsKey(baseStrArr[i])) {
+                stringBuilder.append(sortedMap.get(baseStrArr[i]));
+            } else {
+                stringBuilder.append(baseStrArr[i]);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.smallestEquivalentString("parker", "morris", "parser"));
+        System.out.println(solution.smallestEquivalentString("hello", "world", "hold"));
+        System.out.println(solution.smallestEquivalentString("leetcode", "programs", "sourcecode"));
+    }
+}

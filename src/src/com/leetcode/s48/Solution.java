16932bbcdbb388ba907b456ec366d444c390a3f9
jake.ho <jake.ho@okg.com>
2025-06-09 16:54:54 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s48/Solution.java
+++ src/src/com/leetcode/s48/Solution.java
@@ -1,19 +1,57 @@
 package src.com.leetcode.s48;
+import java.util.Arrays;
 
 class Solution {
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.rotate(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-    }
-
     public void rotate(int[][] matrix) {
-        int numOfRing = Math.max(0, matrix.length - 1);
-        for (int i = 0; i < numOfRing; i++) {
-            System.out.println("i = " + i);
-            for (int x = i; x < (matrix.length - (i + 1)); x++) {
-                System.out.println("x = " + x + " i = " + i);
+        for (int x = 0; x < matrix.length / 2; x++) {
+            int maxT = (matrix.length - x - 1) - (x + 1) + 1;
+            for (int t = 0; t < maxT; t++) {
+                int i = x;
+                int j = x + 1;
+                // left top to right top
+                int nextPixel = matrix[x][x];
+                for (; j < matrix.length - x; j++) {
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = nextPixel;
+                    nextPixel = tmp;
+                }
+                j--;
+
+                // right top to right bottom
+                i++;
+                for (; i < matrix.length - x; i++) {
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = nextPixel;
+                    nextPixel = tmp;
+                }
+                i--;
+
+                // right bottom to left bottom
+                j--;
+                for (; j >= x; j--) {
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = nextPixel;
+                    nextPixel = tmp;
+                }
+                j++;
+
+                // left bottom to left top
+                i--;
+                for (; i >= x; i--) {
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = nextPixel;
+                    nextPixel = tmp;
+                }
+                i++;
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] result = new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16},};
+        solution.rotate(result);
+        System.out.println(Arrays.deepToString(result));
     }
 }

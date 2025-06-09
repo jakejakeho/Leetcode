3a0485df86970ebbfd06f7d54bf7ec6f1f81e3de
jakejakeho <mtho@connect.ust.hk>
2025-06-09 22:15:44 +0800

    Leetcode

--- src/src/com/leetcode/s1143/Solution.java
+++ src/src/com/leetcode/s1143/Solution.java
@@ -1,32 +1,18 @@
 package src.com.leetcode.s1143;
 
-import java.util.Arrays;
-
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] cache = new int[text1.length()][text2.length()];
-        for (int i = 0; i < cache.length; i++) {
-            Arrays.fill(cache[i], -1);
+        int[][] cache = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = text1.length() - 1; i >= 0; i--) {
+            for (int j = text2.length() - 1; j >= 0; j--) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    cache[i][j] = 1 + cache[i + 1][j + 1];
+                } else {
+                    cache[i][j] = Math.max(cache[i + 1][j], cache[i][j + 1]);
+                }
+            }
         }
-        return recursion(text1, text2, cache, 0, 0);
-    }
-
-    private int recursion(String text1, String text2, int[][] cache, int i, int j) {
-        if (i >= text1.length()) {
-            return 0;
-        }
-        if (j >= text2.length()) {
-            return 0;
-        }
-        if (cache[i][j] != -1) {
-            return cache[i][j];
-        }
-        if (text1.charAt(i) == text2.charAt(j)) {
-            return 1 + recursion(text1, text2, cache, i + 1, j + 1);
-        }
-        int longest = Math.max(recursion(text1, text2, cache, i + 1, j), recursion(text1, text2, cache, i, j + 1));
-        cache[i][j] = longest;
-        return longest;
+        return cache[0][0];
     }
 
     public static void main(String[] args) {

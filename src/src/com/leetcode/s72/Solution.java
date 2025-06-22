a1d01cdebe72ae25718175f13f826675d566d7f6
jake.ho <jake.ho@okg.com>
2025-06-22 17:59:43 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s72/Solution.java
+++ src/src/com/leetcode/s72/Solution.java
@@ -4,40 +4,46 @@ import java.util.Arrays;
 class Solution {
 
     public int minDistance(String word1, String word2) {
-        int[][] cache = new int[word1.length()][word2.length()];
-        for (int i = 0; i < cache.length; i++) {
-            Arrays.fill(cache[i], -1);
+        int[][] dp = new int[word1.length()][word2.length()];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return recursion(word1, word2, cache, 0, 0);
+        return recursion(word1, word2, dp, 0, 0);
     }
 
-    private Integer recursion(String word1, String word2, int[][] cache, int i, int j) {
+    private int recursion(String word1, String word2, int[][] dp, int i, int j) {
         if (i >= word1.length()) {
             return word2.length() - j;
         }
         if (j >= word2.length()) {
             return word1.length() - i;
         }
-        if (cache[i][j] != -1) {
-            return cache[i][j];
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
-        int min = 1000000;
+        int minDistance = 1000000;
         if (word1.charAt(i) == word2.charAt(j)) {
-            min = recursion(word1, word2, cache, i + 1, j + 1);
-        } else {
-            // delete
-            min = Math.min(min, 1 + recursion(word1, word2, cache, i + 1, j));
-            // insert
-            min = Math.min(min, 1 + recursion(word1, word2, cache, i, j + 1));
-            // replace
-            min = Math.min(min, 1 + recursion(word1, word2, cache, i + 1, j + 1));
+            minDistance = recursion(word1, word2, dp, i + 1, j + 1);
+            dp[i][j] = minDistance;
+            return minDistance;
         }
-        cache[i][j] = min;
-        return min;
+
+        //insert a character
+        minDistance = Math.min(minDistance, 1 + recursion(word1, word2, dp, i, j + 1));
+
+        // delete a character
+        minDistance = Math.min(minDistance, 1 + recursion(word1, word2, dp, i + 1, j));
+
+        // replace a character
+        minDistance = Math.min(minDistance, 1 + recursion(word1, word2, dp, i + 1, j + 1));
+
+        dp[i][j] = minDistance;
+        return minDistance;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.minDistance("horse", "ros"));
+        System.out.println(solution.minDistance("intention", "execution"));
     }
 }

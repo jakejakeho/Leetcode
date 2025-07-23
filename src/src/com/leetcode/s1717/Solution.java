e3d1b0b0063928e8fd0af085331822d5a4009619
jake.ho <jake.ho@okg.com>
2025-07-23 18:17:22 +0800

    LeetCode
    Signed-off-by: jake.ho <jake.ho@okg.com>

--- src/src/com/leetcode/s1717/Solution.java
+++ src/src/com/leetcode/s1717/Solution.java
@@ -1,41 +1,44 @@
 package src.com.leetcode.s1717;
 
 class Solution {
-    public int maximumGain(String s, int x, int y) {
-        int maxGain = 0;
-        int abIndex = -1;
-        int baIndex = -1;
-        StringBuilder sb = new StringBuilder(s);
-        for (int i = sb.length() - 2; i >= 0; i--) {
-            if (sb.charAt(i) == 'a' && sb.charAt(i) == 'b') {
-                abIndex = i;
-            } else if (sb.charAt(i) == 'b' && sb.charAt(i) == 'a') {
-                baIndex = i;
-            }
-            if (x == y) {
-                if (abIndex != -1) {
-                    sb.deleteCharAt(abIndex);
-                    sb.deleteCharAt(abIndex);
-                    maxGain += x;
-                    abIndex = -1;
-                    baIndex = -1;
-                } else if (baIndex != -1) {
-                    sb.deleteCharAt(baIndex);
-                    sb.deleteCharAt(baIndex);
-                    maxGain += y;
-                    abIndex = -1;
-                    baIndex = -1;
-                }
-            } else if (x > y) {
 
+    public int maximumGain(String s, int x, int y) {
+        int totalScore = 0;
+        StringBuilder sb = new StringBuilder(s);
+
+        char[][] checkLogics = x >= y ? new char[][] {{'a', 'b'}, {'b', 'a'}} : new char[][] {{'b', 'a'}, {'a', 'b'}};
+
+        for (int i = 0; i < sb.length() - 1; i++) {
+            char firstChar = checkLogics[0][0];
+            char secondChar = checkLogics[0][1];
+            if (sb.charAt(i) == firstChar && sb.charAt(i + 1) == secondChar) {
+                totalScore += Math.max(x, y);
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i);
+                i = Math.max(-1, i - 2);
+            }
+
+        }
+
+        for (int i = 0; i < sb.length() - 1; i++) {
+            char firstChar = checkLogics[1][0];
+            char secondChar = checkLogics[1][1];
+            if (sb.charAt(i) == firstChar && sb.charAt(i + 1) == secondChar) {
+                totalScore += Math.min(x, y);
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i);
+                i = Math.max(-1, i - 2);
             }
         }
-        return maxGain;
+        return totalScore;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.maximumGain("cdbcbbaaabab", 4, 5));
-        System.out.println(solution.maximumGain("aabbaaxybbaabb", 5, 4));
+        System.out.println(solution.maximumGain("cdbcbbaaabab", 4, 5)); // Expected: 19
+        System.out.println(solution.maximumGain("aabbaaxybbaabb", 5, 4)); // Expected: 20
+        System.out.println(solution.maximumGain(
+            "aabbrtababbabmaaaeaabeawmvaataabnaabbaaaybbbaabbabbbjpjaabbtabbxaaavsmmnblbbabaeuasvababjbbabbabbasxbbtgbrbbajeabbbfbarbagha",
+            8484, 4096)); // Expected: large value
     }
 }

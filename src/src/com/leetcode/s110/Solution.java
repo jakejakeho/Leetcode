2f2f27725aa0c00625efc2da1feb31c1cece2890
jakejakeho <mtho@connect.ust.hk>
2026-02-08 13:32:03 +0800

    Leetcode

--- src/src/com/leetcode/s110/Solution.java
+++ src/src/com/leetcode/s110/Solution.java
@@ -2,7 +2,7 @@ package src.com.leetcode.s110;
 
 public class Solution {
 
-    public class TreeNode {
+    public static class TreeNode {
 
         int val;
 
@@ -10,32 +10,14 @@ public class Solution {
 
         TreeNode right;
 
-        TreeNode() {
-        }
-
         TreeNode(int val) {
             this.val = val;
         }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
     }
 
-    public boolean isBalanced = true;
 
     public boolean isBalanced(TreeNode root) {
-        isBalanced = true;
-        if (root == null) {
-            return true;
-        }
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-        int diff = rightHeight - leftHeight;
-        isBalanced &= (diff >= -1 && diff <= 1);
-        return isBalanced;
+        return height(root) != -1;
     }
 
     public int height(TreeNode root) {
@@ -43,9 +25,31 @@ public class Solution {
             return 0;
         }
         int leftHeight = height(root.left);
+        if (leftHeight == -1) return -1;
         int rightHeight = height(root.right);
-        int diff = rightHeight - leftHeight;
-        isBalanced &= (diff >= -1 && diff <= 1);
-        return 1 + (Math.max(leftHeight, rightHeight));
+        if (rightHeight == -1) return -1;
+        if (Math.abs(rightHeight - leftHeight) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(solution.isBalanced(root));
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(2);
+        root2.right = new TreeNode(2);
+        root2.left.right = new TreeNode(3);
+        root2.left.left = new TreeNode(3);
+        root2.left.left.right = new TreeNode(4);
+        root2.left.left.left = new TreeNode(4);
+        System.out.println(solution.isBalanced(root2));
     }
 }
